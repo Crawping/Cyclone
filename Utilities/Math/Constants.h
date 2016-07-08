fa4ecb7a87dc80f0bd535fcd7477887352b24957
JoshGrooms <josh.grooms@gmail.com>
2016-07-08 13:21:12 -0700

    Created a new file to hold numeric constants.

---
+++ Utilities/Math/Constants.h
@@ -0,0 +1,18 @@
+/* CHANGELOG 
+ * Written by Josh Grooms on 20160708
+ */
+
+#pragma once
+
+
+
+namespace Cyclone::Utilities::Constants
+{
+    const double E          = 2.71828182845904523536;
+    const double Pi         = 3.14159265358979323846;
+
+    const double HalfPi     = Pi / 2.0;
+    const double QuarterPi  = Pi / 4.0;
+    const double ThirdPi    = Pi / 3.0;
+    const double TwoPi      = 2 * Pi;
+}
