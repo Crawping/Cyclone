c8b6a2df6060f571fe75451244d91ed1c0785948
JoshGrooms <josh.grooms@gmail.com>
2016-07-10 07:19:54 -0700

    Removed some unnecessary code.

--- Testing/Main.cpp
+++ Testing/Main.cpp
@@ -6,16 +6,9 @@
 #include <iostream>
 
 
-//#include "_Color4.cpp"
-
-
-__declspec(dllexport) int PullInMyLibrary() { return 0; }
-
 
 int main(int nargs, char** args)
 {
-    static int notused = PullInMyLibrary();
-
     ::testing::InitGoogleTest(&nargs, args);
     return RUN_ALL_TESTS();
 }
