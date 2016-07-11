d7ff599d515b7e10ec3a4df6972c5a7f6e7d4dd0
JoshGrooms <josh.grooms@gmail.com>
2016-07-11 11:17:55 -0700

    Implemented an internal API macro.
    This really only matters for Linux builds.

--- Platform/PlatformAPI.h
+++ Platform/PlatformAPI.h
@@ -7,12 +7,14 @@
 
 
 #ifdef __linux__
+    #define InternalAPI     __attribute__(visibility("hidden"))
     #ifdef PLATFORM_EXPORTS
         #define PlatformAPI __attribute__(visibility("default"))
     #else
         #define PlatformAPI
     #endif
 #else
+    #define InternalAPI
     #ifdef PLATFORM_EXPORTS
         #define PlatformAPI __declspec(dllexport)
     #else
