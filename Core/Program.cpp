34eadaa9e0ded56229e80d7a929aa79feeaf87ef
JoshGrooms <josh.grooms@gmail.com>
2016-07-15 09:42:55 -0700

    Started implementing an overarching rendering controller.

--- Core/Program.cpp
+++ Core/Program.cpp
@@ -5,9 +5,16 @@
 using namespace Cyclone::Platform;
 using namespace Cyclone::Utilities;
 
+const static string Help = "            \n\
+    CYCLONE - A cross - platform 3D rendering engine. \n\n";
 
 
-Program::Program(int nargs, char** args)
+
+/** CONSTRUCTOR & DESTRUCTOR **/
+Program::Program(int nargs, char** args) : 
+    _debug(false),
+    _display(0),
+    _showHelp(false)
 {
     ParseInputArguments(nargs, args);
     
@@ -15,18 +22,45 @@ Program::Program(int nargs, char** args)
         Console::WriteLine("Failed to load the OpenGL function pointers.");
 }
 
-
 Program::~Program()
 {
     cglClearAPI();
 }
 
-void Program::InitializeOpenGL()
+
+
+/** UTILITIES **/
+void Program::Execute()
 {
-    
+
 }
 
 void Program::ParseInputArguments(int nargs, char** args)
 {
+    if (nargs == 1)
+        return;
 
+    string ctArg, ntArg;
+    for (int a = 1; a < nargs; a++)
+    {
+        ctArg = args[a];
+        if (ctArg == "-h" || ctArg == "--help")
+            _showHelp = true;
+        else if (ctArg == "--debug")
+            _debug = true;
+        else if (ctArg == "--display")
+        {
+            if (a < nargs - 1)
+            {
+                ntArg = args[++a];
+                if (ntArg[0] == ':')
+                    ntArg.erase(ntArg.begin());
+                _display = std::stoi(ntArg);
+            }
+            else
+                Console::WriteLine("The display option was invoked at the command line, but no display index was provided.");
+        }
+        else
+            Console::WriteLine("Unrecognized command line input argument: " + ctArg);
+    }
 }
