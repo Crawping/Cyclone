d91dea51c9a3b3db5677a57df0203a0466674f0d
JoshGrooms <josh.grooms@gmail.com>
2016-07-19 08:14:32 -0700

    Added some documentation.

--- Utilities/Math/Constants.h
+++ Utilities/Math/Constants.h
@@ -10,14 +10,28 @@ namespace Cyclone
 {
     namespace Utilities
     {
+        /// <summary> A list of commonly used constant numeric values. </summary>
         namespace Constants
         {
+            /// <summary> The double-precision mathematical constant e. </summary>
+            /// <remarks> The constant e is also sometimes referred to as Euler's number. </remarks>
             const double E          = 2.71828182845904523536;
+            /// <summary> The double-precision mathematical constant pi. </summary>
+            /// <remarks> When considered as an angle in radians, pi is equivalent to 180 degrees. </remarks>
             const double Pi         = 3.14159265358979323846;
 
+
+            /// <summary> The double-precision mathematical constant pi / 2.0. </summary>
+            /// <remarks> When considered as an angle in radians, pi/2 is equivalent to 90 degrees. </remarks>
             const double HalfPi     = Pi / 2.0;
+            /// <summary> The double-precision mathematical constant pi / 4.0. </summary>
+            /// <remarks> When considered as an angle in radians, pi/4 is equivalent to 45 degrees. </remarks>
             const double QuarterPi  = Pi / 4.0;
+            /// <summary> The double-precision mathematical constant pi / 3.0. </summary>
+            /// <remarks> When considered as an angle in radians, pi/3 is equivalent to 60 degrees. </remarks>
             const double ThirdPi    = Pi / 3.0;
+            /// <summary> The double-precision mathematical constant pi * 2.0. </summary>
+            /// <remarks> When considered as an angle in radians, 2*pi is equivalent to 360 degrees. </remarks>
             const double TwoPi      = 2 * Pi;
         }
     }
