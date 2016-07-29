b5ed36d5cb425d63a1e080991418e9a613afe215
JoshGrooms <josh.grooms@gmail.com>
2016-07-29 07:19:39 -0700

    Wrapped class in namespaces and added/converted documentation.
    Somehow, I forgot to wrap this class in namespaces when I went
    through the library and did that for everything else a while back.

--- OpenGL/Pipelines/Shader.cpp
+++ OpenGL/Pipelines/Shader.cpp
@@ -9,93 +9,97 @@
 using std::string;
 
 
-
-/** CONSTRUCTOR & DESTRUCTOR **/
-Shader::Shader(string fileName, ShaderTypes type) :
-    _id(glCreateShader(type)),
-    _location(fileName),
-    _type(type)
+namespace Cyclone
 {
-    string src = readfile(_location);
-    if (src.size() == 0)
-        Console::WriteLine("Unable to open and read the shader program file " + _location);
-    else
+    namespace OpenGL
     {
-        src = Preprocess(src);
-        Compile(src);
-    }
-}
-Shader::~Shader()
-{
-    if (_id)
-        glDeleteShader(_id);
-}
-
-
-
-/** PRIVATE UTILITIES **/
-/* COMPILE - Compiles the GLSL source code for this shader into a binary package.
- *
- *      Compiled shader programs can be linked together to form a shader program pipeline.
- */
-void Shader::Compile(string src)
-{
-    const char* srcstr = src.c_str();
-    glShaderSource(ID(), 1, &srcstr, NULL);
-
-    GLint status;
-    glCompileShader(ID());
-    glGetShaderiv(ID(), GL_COMPILE_STATUS, &status);
-
-    if (status == GL_FALSE)
-    {
-        Console::WriteLine("Failed to compile the shader program in " + _location);
-        Console::WriteLine("GPU Error Information: " + ReportShaderLog());
-    }
-}
-
-/* PREPROCESS - Edits the GLSL source code as necessary before passing it on to the shader program compiler.
- *
- *      This method provides a customizable preprocessing stage for GLSL source code, allowing for additional feature support
- *      within those files. Currently, it is being used to provide basic support for #include directives referencing other
- *      files.
- */
-string Shader::Preprocess(string src)
-{
-    const static std::regex incPattern("\\s*#\\s*include\\s+\"(.*)\"");
-    const string path(SourcePath().substr(0, SourcePath().find_last_of("/")));
-
-    std::smatch incMatches;
-    std::stringstream prepsrc;
-    while (std::regex_search(src, incMatches, incPattern))
-    {
-        prepsrc << incMatches.prefix().str();
-
-        string filestr = readfile(path + "/" + incMatches[1].str());
-        if (filestr.size() == 0)
+        /** CONSTRUCTOR & DESTRUCTOR **/
+        Shader::Shader(string fileName, ShaderTypes type) :
+            _id(glCreateShader(type)),
+            _location(fileName),
+            _type(type)
         {
-            Console::WriteLine("Unable to locate the file \"" + incMatches[1].str() + "\" for inclusion in this shader.");
-            return src;
+            string src = readfile(_location);
+            if (src.size() == 0)
+                Console::WriteLine("Unable to open and read the shader program file " + _location);
+            else
+            {
+                src = Preprocess(src);
+                Compile(src);
+            }
+        }
+        Shader::~Shader()
+        {
+            if (_id)
+                glDeleteShader(_id);
         }
 
-        prepsrc << Preprocess(filestr);
-        src = incMatches.suffix().str();
-    }
 
-    prepsrc << src;
-    return prepsrc.str();
-}
 
-/* POSTSHADERLOG - Retrieves and posts error information that was logged while attempting to compile this shader program. */
-string Shader::ReportShaderLog()
-{
-    GLint nchars = 0;
-    glGetShaderiv(ID(), GL_INFO_LOG_LENGTH, &nchars);
-    if (nchars > 0)
-    {
-        GLchar msg[1024];
-        glGetShaderInfoLog(ID(), nchars, 0, msg);
-        return msg;
+        /** PRIVATE UTILITIES **/
+        /// <summary> Compiles the GLSL source code for this shader into a binary package. </summary>
+        /// <remarks> Compiled shader programs can be linked together to form a shader program pipeline. </remarks>
+        void Shader::Compile(string src)
+        {
+            const char* srcstr = src.c_str();
+            glShaderSource(ID(), 1, &srcstr, NULL);
+
+            GLint status;
+            glCompileShader(ID());
+            glGetShaderiv(ID(), GL_COMPILE_STATUS, &status);
+
+            if (status == GL_FALSE)
+            {
+                Console::WriteLine("Failed to compile the shader program in " + _location);
+                Console::WriteLine("GPU Error Information: " + ReportShaderLog());
+            }
+        }
+
+        /// <summary> Edits the GLSL source code as necessary before passing it on to the shader program compiler. </summary>
+        /// <remarks>
+        ///     This method provides a customizable preprocessing stage for GLSL source code, allowing for additional feature support
+        ///     within those files. Currently, it is being used to provide basic support for #include directives referencing other
+        ///     files.
+        /// </remarks>
+        string Shader::Preprocess(string src)
+        {
+            const static std::regex incPattern("\\s*#\\s*include\\s+\"(.*)\"");
+            const string path(SourcePath().substr(0, SourcePath().find_last_of("/")));
+
+            std::smatch incMatches;
+            std::stringstream prepsrc;
+            while (std::regex_search(src, incMatches, incPattern))
+            {
+                prepsrc << incMatches.prefix().str();
+
+                string filestr = readfile(path + "/" + incMatches[1].str());
+                if (filestr.size() == 0)
+                {
+                    Console::WriteLine("Unable to locate the file \"" + incMatches[1].str() + "\" for inclusion in this shader.");
+                    return src;
+                }
+
+                prepsrc << Preprocess(filestr);
+                src = incMatches.suffix().str();
+            }
+
+            prepsrc << src;
+            return prepsrc.str();
+        }
+
+        /// <summary> Retrieves and posts error information that was logged while attempting to compile this shader program. </summary>
+        string Shader::ReportShaderLog()
+        {
+            GLint nchars = 0;
+            glGetShaderiv(ID(), GL_INFO_LOG_LENGTH, &nchars);
+            if (nchars > 0)
+            {
+                GLchar msg[1024];
+                glGetShaderInfoLog(ID(), nchars, 0, msg);
+                return msg;
+            }
+            return "No errors were detected.";
+        }
+
     }
-    return "No errors were detected.";
 }
--- OpenGL/Pipelines/Shader.h
+++ OpenGL/Pipelines/Shader.h
@@ -9,63 +9,58 @@
 #include "TypeDefinitions.h"
 
 
-
-/// <summary> A class that creates and manages one individual GPU shader program. </summary>
-/// <remarks>
-///		The shader class is responsible for creating compiled binaries from OpenGL Shading Language(GLSL) source code.
-///		Multiple instances of these binaries, each representing a discrete stage of the programmable GPU rendering pipeline,
-///		can then be linked together to form a complete program useful for rendering geometry.Typically, this process is
-///	    handled automatically within the 'ShaderPipeline' class.
-/// </remarks>
-class Shader
+namespace Cyclone
 {
-    public:
+    namespace OpenGL
+    {
+        /// <summary> A class that creates and manages one individual GPU shader program. </summary>
+        /// <remarks>
+        ///		The shader class is responsible for creating compiled binaries from OpenGL Shading Language(GLSL) source code.
+        ///		Multiple instances of these binaries, each representing a discrete stage of the programmable GPU rendering pipeline,
+        ///		can then be linked together to form a complete program useful for rendering geometry.Typically, this process is
+        ///	    handled automatically within the 'ShaderPipeline' class.
+        /// </remarks>
+        class Shader
+        {
+            public:
 
-        /** PROPERTIES **/
-        /// <summary> The numeric handle representing the shader program on the GPU. </summary>
-		OpenGLAPI uint ID()                   const { return _id; }
-		/// <summary> Gets the full path string pointing to the GLSL source code file for this shader program. </summary>
-		OpenGLAPI const string& SourcePath()  const { return _location; }
-		/// <summary> The stage of the GPU rendering pipeline to which this program belongs. </summary>
-		OpenGLAPI ShaderTypes Type()          const { return _type; }
-
-
-
-        /** CONSTRUCTOR & DESTRUCTOR **/
-        /* SHADER - Constructs a new shader program that can be plugged into the programmable GPU rendering stages.
-         *
-         * SYNTAX:
-         *      S = Shader(fileName, type)
-         *
-         * OUTPUT:
-         *      S:          Shader
-         *
-         * INPUTS:
-         *      fileName:   string
-         *                  A string containing the name or path of the shader program source code file to be compiled.
-         *
-         *      type:       ShaderTypes
-         *                  One of the shader type enumerators. This argument dictates where in the GPU pipeline the compiled
-         *                  shader program will be
-         *                  compiled program
-         */
-		OpenGLAPI Shader(string fileName, ShaderTypes type);
-		OpenGLAPI ~Shader();
+                /** PROPERTIES **/
+                /// <summary> The numeric handle representing the shader program on the GPU. </summary>
+		        OpenGLAPI uint ID()                   const { return _id; }
+		        /// <summary> Gets the full path string pointing to the GLSL source code file for this shader program. </summary>
+		        OpenGLAPI const string& SourcePath()  const { return _location; }
+		        /// <summary> The stage of the GPU rendering pipeline to which this program belongs. </summary>
+		        OpenGLAPI ShaderTypes Type()          const { return _type; }
 
 
 
-    private:
+                /** CONSTRUCTOR & DESTRUCTOR **/
+                /// <summary> Constructs a new shader program that can be plugged into the programmable GPU rendering stages. </summary>
+                /// <param name="fileName"> A string containing the name or path of the shader program source code file to be compiled. </param>
+                /// <param name="type">
+                ///     One of the shader type enumerators. This argument dictates where in the GPU pipeline the compiled
+                ///     shader program will be.
+                /// </param>
+		        OpenGLAPI Shader(string fileName, ShaderTypes type);
+		        OpenGLAPI ~Shader();
 
-        /** PROPERTY DATA **/
-        uint            _id;
-        string          _location;
-        ShaderTypes     _type;
+
+
+            private:
+
+                /** PROPERTY DATA **/
+                uint            _id;
+                string          _location;
+                ShaderTypes     _type;
 
 
 
-        /** PRIVATE UTILITIES **/
-        void Compile(string src);
-        string Preprocess(string src);
-        string ReportShaderLog();
+                /** PRIVATE UTILITIES **/
+                void Compile(string src);
+                string Preprocess(string src);
+                string ReportShaderLog();
 
-};
+        };
+
+    }
+}
