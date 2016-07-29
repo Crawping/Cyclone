e5e4db420cef65b432164ac88c53f4aa97172f45
JoshGrooms <josh.grooms@gmail.com>
2016-07-29 07:22:00 -0700

    Attached export symbols to public methods.
    Before, there just one export symbol attached to the whole class,
    which is bad practice.

--- Utilities/Spatial/Transform.h
+++ Utilities/Spatial/Transform.h
@@ -16,7 +16,7 @@ namespace Cyclone
         struct Volume;
 
 
-        struct UtilitiesAPI Transform
+        struct Transform
         {
             public:
                 /** PROPERTIES **/
@@ -66,11 +66,11 @@ namespace Cyclone
 
                 /** CONSTRUCTORS **/
                 /// <summary> Constructs a default transformation data structure representing a 4x4 identity matrix. </summary>
-                Transform();
+                UtilitiesAPI Transform();
                 /// <summary> Constructs a new transformation data structure that is a deep copy of another one. </summary>
-                Transform(const Transform& other);
+                UtilitiesAPI Transform(const Transform& other);
                 /// <summary> Constructs a transformation data structure by copying an existing 4x4 transformation matrix. </summary>
-                Transform(const float* m);
+                UtilitiesAPI Transform(const float* m);
                 /// <summary> Constructs a transformation matrix representing translation, rotation, and scaling in 3D space. </summary>
                 /// <param name="position"> A three-element vector containing the desired translations along the X, Y, and Z axes. </param>
                 /// <param name="size">
@@ -85,7 +85,7 @@ namespace Cyclone
                 ///     A new data structure representing a transformation matrix that can be multiplied by a set of
                 ///     coordinates to translate, rotate, and scale them in 3D space.
                 /// </returns>
-                Transform(const Vector3& position, const Vector3& size = Vector3::One, const Vector3& angles = Vector3::Zero);
+                UtilitiesAPI Transform(const Vector3& position, const Vector3& size = Vector3::One, const Vector3& angles = Vector3::Zero);
 
 
 
@@ -108,7 +108,7 @@ namespace Cyclone
                  */
                 
                 /// <summary> Constructs a right-handed perspective projection transformation matrix. </summary>
-                static Transform PerspectiveProjection(const Volume& displayVolume);
+                UtilitiesAPI static Transform PerspectiveProjection(const Volume& displayVolume);
                 /* PERSPECTIVEPROJECTION - Constructs a right-handed perspective projection transformation matrix.
                  *
                  *      SYNTAX:
@@ -137,31 +137,31 @@ namespace Cyclone
                  *                      The desired position of the clipping plane furthest from the camera in view-space.
                  */
                  /// <summary> Constructs a right-handed perspective projection transformation matrix. </summary>
-                static Transform PerspectiveProjection(float fov, float aspect, float znear, float zfar);
-                static Transform Rotation(const Vector3& angles);
-                static Transform Rotation(float roll, float pitch, float yaw);
-                static Transform Scaling(const Vector3& size);
-                static Transform Scaling(float x, float y, float z);
-                static Transform Translation(const Vector3& position);
-                static Transform Translation(float x, float y, float z);
+                UtilitiesAPI static Transform PerspectiveProjection(float fov, float aspect, float znear, float zfar);
+                UtilitiesAPI static Transform Rotation(const Vector3& angles);
+                UtilitiesAPI static Transform Rotation(float roll, float pitch, float yaw);
+                UtilitiesAPI static Transform Scaling(const Vector3& size);
+                UtilitiesAPI static Transform Scaling(float x, float y, float z);
+                UtilitiesAPI static Transform Translation(const Vector3& position);
+                UtilitiesAPI static Transform Translation(float x, float y, float z);
 
 
 
                 /** UTILITIES **/
                 /// <summary> Generates a human-readable string detailing the current internal state of this data structure. </summary>
-                string Report() const;
+                UtilitiesAPI string Report() const;
                 /// <summary> Converts a transformation data structure into a native vector of values. </summary>
-                const float* ToArray()                          const { UpdateState(); return State.ToArray(); }
+                UtilitiesAPI const float* ToArray()                          const { UpdateState(); return State.ToArray(); }
                 /// <summary> Sets the translation components of the transformation matrix relative to their current values. </summary>
-                Transform& Translate(const Vector3& t)                { return Position(_position + t); }
+                UtilitiesAPI Transform& Translate(const Vector3& t)                { return Position(_position + t); }
                 /// <summary> Sets the translation components of the transformation matrix relative to their current values. </summary>
-                Transform& Translate(float x, float y, float z)       { return Translate(Vector3(x, y, z)); }
+                UtilitiesAPI Transform& Translate(float x, float y, float z)       { return Translate(Vector3(x, y, z)); }
 
 
 
                 /** OPERATORS **/
-                bool operator ==(const Transform& other)        const;
-                bool operator !=(const Transform& other)        const { return !(*this == other); }
+                UtilitiesAPI bool operator ==(const Transform& other)        const;
+                UtilitiesAPI bool operator !=(const Transform& other)        const { return !(*this == other); }
 
 
 
@@ -198,6 +198,6 @@ namespace Cyclone
 
 
         /// <summary> Performs matrix multiplication for two 4x4 transformation matrices. </summary>
-        Transform operator *(Transform left, Transform right);
+        UtilitiesAPI Transform operator *(Transform left, Transform right);
     }
 }
