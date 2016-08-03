d784065d2266db1fe4859f98dfba85f8ffaadb25
JoshGrooms <josh.grooms@gmail.com>
2016-08-03 08:59:33 -0700

    Added basic right-triangle geometry.

--- Utilities/Spatial/Geometry.h
+++ Utilities/Spatial/Geometry.h
@@ -3,9 +3,9 @@
  */
 
 #pragma once
-#include "Array.h"
 #include "UtilitiesAPI.h"
-#include "Vertex.h"
+#include "Math/Array.h"
+#include "Spatial/Vertex.h"
 
 
 
@@ -17,6 +17,14 @@ namespace Cyclone
         {
             /// <summary> A single point in three dimensional space at the coordinates (0, 0, 0). </summary>
 	        const Array<Vertex> Point = { Vertex() };
+            /// <summary> A right-triangle in the XY plane whose hypotenuse spans the coordinates (1, 0, 0) to (0, 1, 0). </summary>
+            /// <remarks> The vertices of this triangle are wound counter-clockwise. </remarks>
+            const Array<Vertex> Triangle =
+            {
+                Vertex(0.0f, 0.0f, 0.0f, 0.0f, 0.0f),
+                Vertex(1.0f, 0.0f, 0.0f, 1.0f, 0.0f),
+                Vertex(0.0f, 1.0f, 0.0f, 0.0f, 1.0f),
+            };
 	        /// <summary> A unit quad in the XY plane spanning the coordinates (0, 0, 0) to (1, 1, 0). </summary>
 	        /// <remarks> The vertices of this quad are not shared and are wound counter-clockwise. </remarks>
 	        const Array<Vertex> Quad =
