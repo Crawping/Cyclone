53b796ceafba99d352a54323d87a6b3a6df577b9
JoshGrooms <josh.grooms@gmail.com>
2016-08-05 08:34:02 -0700

    Added and updated some documentation.

--- OpenGL/Buffers/VertexBuffer.h
+++ OpenGL/Buffers/VertexBuffer.h
@@ -43,17 +43,22 @@ namespace Cyclone
 		        /// <summary> Attaches this buffer to the active vertex shader on the GPU. </summary>
 		        /// <remarks>
 		        ///	    This method binds the vertex array object (VAO) to the GPU, but does not enable any of the vertex attributes
-		        ///     (<see cref="BindResources"/> takes care of this).Typically, this will be called through the 'Bind' method. However, 
+		        ///     (<see cref="BindResources"/> takes care of this). Typically, this will be called through the 'Bind' method. However, 
 		        ///     it is provided here because there are times when only the VAO should be enabled without also enabling its attributes.
 		        /// </remarks>
 		        OpenGLAPI void BindEntity(int slot = 0)   const override;
                 /// <summary> Attaches resources within this buffer to their corresponding variables in the vertex shader. </summary>
+                /// <remarks>
+                ///     This method enables the attributes of the vertex buffer on the GPU, which essentially tells the rendering device how 
+                ///     to interpret the data inside of the buffer. Typically, this action will be performed automatically when calling the 
+                ///     <see cref="Bind"/> method.
+                /// </remarks>
 		        OpenGLAPI void BindResources()            const override;
 		        /// <summary> Detaches this buffer and its resources from the GPU rendering pipeline. </summary>
 		        OpenGLAPI void Unbind()                   const override;
-
+                /// <summary> Detaches this buffer from the active vertex shader on the GPU. </summary>
 		        OpenGLAPI void UnbindEntity()             const override;
-
+                /// <summary> Detaches any secondary resources associated with this buffer from the GPU. </summary>
 		        OpenGLAPI void UnbindResources()          const override;
 
 
