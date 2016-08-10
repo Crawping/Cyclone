148b9a10e31e2c08924e9f43605a1b06f609e266
JoshGrooms <josh.grooms@gmail.com>
2016-08-10 08:06:51 -0700

    Created a new entity representing a line in 3D space.

---
+++ OpenGL/Primitives/Line3D.h
@@ -0,0 +1,21 @@
+/* CHANGELOG
+ * Written by Josh Grooms on 20160810
+ */
+
+#pragma once
+#include "Primitives/Entity3D.h"
+#include "Spatial/Geometry.h"
+
+
+
+namespace Cyclone
+{
+    namespace OpenGL
+    {
+        class Line3D : public Entity3D
+        {
+            public: 
+                Line3D() : Entity3D(Geometry::Quad) { }
+        };
+    }
+}
