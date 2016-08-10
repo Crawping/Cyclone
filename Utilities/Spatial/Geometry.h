5ae166ed0db1200cb167b6e9aac75dd56482f95e
JoshGrooms <josh.grooms@gmail.com>
2016-08-10 08:02:53 -0700

    Implemented geometry for basic unit cubes.

--- Utilities/Spatial/Geometry.h
+++ Utilities/Spatial/Geometry.h
@@ -15,6 +15,62 @@ namespace Cyclone
     {
         namespace Geometry
         {
+            const Array<Vertex> Cube =
+            {
+                // Front
+                Vertex(0.0f, 0.0f, 0.0f, 0.0f, 0.0f),
+                Vertex(1.0f, 0.0f, 0.0f, 1.0f, 0.0f),
+                Vertex(0.0f, 1.0f, 0.0f, 0.0f, 1.0f),
+
+                Vertex(0.0f, 1.0f, 0.0f, 0.0f, 1.0f),
+                Vertex(1.0f, 0.0f, 0.0f, 1.0f, 0.0f),
+                Vertex(1.0f, 1.0f, 0.0f, 1.0f, 1.0f),
+
+                // Top
+                Vertex(0.0f, 1.0f, 0.0f, 0.0f, 0.0f),
+                Vertex(1.0f, 1.0f, 0.0f, 1.0f, 0.0f),
+                Vertex(0.0f, 1.0f, 1.0f, 0.0f, 1.0f),
+
+                Vertex(0.0f, 1.0f, 1.0f, 0.0f, 1.0f),
+                Vertex(1.0f, 1.0f, 0.0f, 1.0f, 0.0f),
+                Vertex(1.0f, 1.0f, 1.0f, 1.0f, 1.0f),
+
+                // Bottom
+                Vertex(0.0f, 0.0f, 1.0f, 0.0f, 0.0f),
+                Vertex(1.0f, 0.0f, 1.0f, 1.0f, 0.0f),
+                Vertex(0.0f, 0.0f, 0.0f, 0.0f, 1.0f),
+
+                Vertex(0.0f, 0.0f, 0.0f, 0.0f, 1.0f),
+                Vertex(1.0f, 0.0f, 1.0f, 1.0f, 0.0f),
+                Vertex(1.0f, 0.0f, 0.0f, 1.0f, 1.0f),
+
+                // Left
+                Vertex(0.0f, 0.0f, 1.0f, 0.0f, 0.0f),
+                Vertex(0.0f, 0.0f, 0.0f, 1.0f, 0.0f),
+                Vertex(0.0f, 1.0f, 1.0f, 0.0f, 1.0f),
+
+                Vertex(0.0f, 1.0f, 1.0f, 0.0f, 1.0f),
+                Vertex(0.0f, 0.0f, 0.0f, 1.0f, 0.0f),
+                Vertex(0.0f, 1.0f, 0.0f, 1.0f, 1.0f),
+
+                // Right
+                Vertex(1.0f, 0.0f, 0.0f, 0.0f, 0.0f),
+                Vertex(1.0f, 0.0f, 1.0f, 1.0f, 0.0f),
+                Vertex(1.0f, 1.0f, 0.0f, 0.0f, 1.0f),
+
+                Vertex(1.0f, 1.0f, 0.0f, 0.0f, 1.0f),
+                Vertex(1.0f, 0.0f, 1.0f, 1.0f, 0.0f),
+                Vertex(1.0f, 1.0f, 1.0f, 1.0f, 1.0f),
+
+                // Back
+                Vertex(1.0f, 0.0f, 1.0f, 0.0f, 0.0f),
+                Vertex(0.0f, 0.0f, 1.0f, 1.0f, 0.0f),
+                Vertex(1.0f, 1.0f, 1.0f, 0.0f, 1.0f),
+
+                Vertex(1.0f, 1.0f, 1.0f, 0.0f, 1.0f),
+                Vertex(0.0f, 0.0f, 1.0f, 1.0f, 0.0f),
+                Vertex(0.0f, 1.0f, 1.0f, 1.0f, 1.0f),
+            };
             /// <summary> A single point in three dimensional space at the coordinates (0, 0, 0). </summary>
 	        const Array<Vertex> Point = { Vertex() };
             /// <summary> A right-triangle in the XY plane whose hypotenuse spans the coordinates (1, 0, 0) to (0, 1, 0). </summary>
