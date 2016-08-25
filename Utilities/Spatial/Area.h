77ccb32a5ad1e822fc5d34e8d617898fa8b90741
JoshGrooms <josh.grooms@gmail.com>
2016-08-25 08:00:12 -0700

    Added folder names to include directives.

--- Utilities/Spatial/Area.h
+++ Utilities/Spatial/Area.h
@@ -3,7 +3,7 @@
  */
 
 #pragma once
-#include "Vector2.h"
+#include "Math/Vector2.h"
 
 
 
--- Utilities/Spatial/Vertex.h
+++ Utilities/Spatial/Vertex.h
@@ -3,8 +3,8 @@
  */
 
 #pragma once
-#include "Vector2.h"
-#include "Vector3.h"
+#include "Math/Vector2.h"
+#include "Math/Vector3.h"
 
 
 
