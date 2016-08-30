9277c35d7a595353e1b421c807aac9b1e6562e19
JoshGrooms <josh.grooms@gmail.com>
2016-08-30 10:34:01 -0700

    Added a few more tests for vertex layout arrays.

--- Testing/Vertex.cpp
+++ Testing/Vertex.cpp
@@ -11,10 +11,14 @@ class _Vertex : public testing::Test
     protected:
         
         Vertex::Simple _v0;
+        Array<VertexAttribute> _a1;
+        Array<VertexAttribute> _a2;
 
 
 
-        _Vertex()
+        _Vertex() : 
+            _a1(Vertex::Simple().Layout()),
+            _a2(Vertex::Standard().Layout())
         {
 
         }
@@ -32,13 +36,20 @@ TEST_F(_Vertex, DefaultConstruction)
 
 TEST_F(_Vertex, VertexLayouts)
 {
-    auto layout = _v0.Layout();
-    ASSERT_EQ(layout.Count(), 2);
+    ASSERT_EQ(_a1.Count(), 2);
 
-    ASSERT_EQ(layout(0).Count, 3);
-    ASSERT_EQ(layout(0).Format, NumericFormats::Float);
-    ASSERT_EQ(layout(0).Offset, offsetof(Vertex::Simple, Position));
+    ASSERT_EQ(_a1(0).Count, 3);
+    ASSERT_EQ(_a1(0).Format, NumericFormats::Float);
+    ASSERT_EQ(_a1(0).Offset, offsetof(Vertex::Simple, Position));
 
-    ASSERT_EQ(layout(1).Count, 2);
-    ASSERT_EQ(layout(1).Offset, offsetof(Vertex::Simple, UV));
+    ASSERT_EQ(_a1(1).Count, 2);
+    ASSERT_EQ(_a1(1).Offset, offsetof(Vertex::Simple, UV));
+
+    ASSERT_EQ(_a2.Count(), 3);
+    ASSERT_EQ(_a2(0).Count, 3);
+    ASSERT_EQ(_a2(0).Format, NumericFormats::Float);
+    ASSERT_EQ(_a2(0).Offset, offsetof(Vertex::Standard, Position));
+
+    ASSERT_EQ(_a2(1).Count, 3);
+    ASSERT_EQ(_a2(1).Offset, offsetof(Vertex::Standard, Normal));
 }
