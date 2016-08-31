b613f12c8e82a7561024e597427046e40019854f
JoshGrooms <josh.grooms@gmail.com>
2016-08-31 08:43:56 -0700

    Finished documenting this interface.

--- OpenGL/Interfaces/IGraphicsBuffer.h
+++ OpenGL/Interfaces/IGraphicsBuffer.h
@@ -13,16 +13,19 @@ namespace Cyclone
 {
     namespace OpenGL
     {
+        /// <summary> An interface that represents a generalized GPU buffer object. </summary>
         class OpenGLAPI IGraphicsBuffer : public IBindable, public IUpdatable
         {
 
             public:
+                /// <summary> Gets the number of elements that are present in the buffer. </summary>
                 virtual uint Count()        const = 0;
-
+                /// <summary> Gets whether the buffer object is currently empty. </summary>
+                /// <remarks> By default, this method determines whether the returned <see cref="Count"/> value is zero. </remarks>
                 virtual bool IsEmpty()      const { return Count() == 0; }
-
+                /// <summary> Destroys any GPU- or heap-allocated resources held by the buffer. </summary>
                 virtual ~IGraphicsBuffer()  { }
-                
+                /// <summary> Removes all of the data stored within the buffer. </summary>
                 virtual void Clear()        = 0;
 
         };
