8f71fab27c8893e6ca73ce42856b5e111ca0e93f
JoshGrooms <josh.grooms@gmail.com>
2016-09-02 17:16:57 -0700

    Created a demo for indirect rendering of indexed geometry.

---
+++ Demos/IndexedCube/Main.cpp
@@ -0,0 +1,75 @@
+
+#include "BasicRenderer.h"
+#include "GPU.h"
+#include "Window3D.h"
+#include "Geometry/Geometry.h"
+#include "Geometry/Mesh3D.h"
+#include "Geometry/Scene3D.h"
+#include "Pipelines/ShaderPipeline.h"
+
+using namespace Renderers;
+using namespace Cyclone::OpenGL;
+
+
+
+class Program : public BasicRenderer
+{
+    public:
+        Program() : 
+            BasicRenderer("Indexed Cube"),
+            Cube(nullptr)
+        {
+            Array<uint> indices;
+            Array<Vertex::Standard> vertices = Geometry::Cube(indices);
+            Cube = new Mesh3D(vertices, indices);
+
+            Initialize();
+            glEnable(GL_CULL_FACE);
+        }
+        ~Program()
+        {
+            if (Cube) { delete Cube; }
+        }
+
+    protected:
+        Mesh3D* Cube;
+
+        void CreateSceneResources() override
+        {
+            BasicRenderer::CreateSceneResources();
+            Cube->Scale(100, 100, 100)
+                .Color(Color4::Green)
+                .Position(RenderWindow->ClientArea().Center())
+                .Pitch(90)
+                .Roll(90);
+            RenderScene->Add(*Cube);
+        }
+
+        void CreateSizedResources() override
+        {
+            BasicRenderer::CreateSizedResources();
+            Cube->Position(Vector3(RenderWindow->ClientArea().Center(), 50));
+        }
+
+        void CreateShaderPipeline() override
+        {
+            RenderPipeline = new ShaderPipeline("../Renderers/Shaders/Default.vsl", "../Renderers/Shaders/Depth.psl");
+            Renderer->RenderPipeline(RenderPipeline);
+        }
+
+        void UpdateScene() override
+        {
+            Cube->Rotate(Vector3(0.0f, 0.01f, 0.0f));
+            RenderScene->Add(*Cube);
+            BasicRenderer::UpdateScene();
+        }
+};
+
+
+
+int main(int nargs, char** args)
+{
+    Program app;
+    app.Execute();
+    return 0;
+}
