b0c615333c1f03b7bdeb817d67cf81e24cd3c512
JoshGrooms <josh.grooms@gmail.com>
2016-09-13 07:45:50 -0700

    Drafted some property documentation to explain point sizing in OpenGL.

--- OpenGL/Geometry/Point3D.h
+++ OpenGL/Geometry/Point3D.h
@@ -23,16 +23,52 @@ namespace Cyclone
 
 
                 /** PROPERTIES **/
+                /// <summary> Sets the size of the point along the world space z-axis. </summary>
+                /// <remarks>
+                ///     Currently, points in OpenGL may only have a uniform size in all three spatial dimensions, unless 
+                ///     custom-written shaders are available to produce different effects. Thus, attempting to set the 
+                ///     scale of a point along any world-space axis will result in that value being copied over to the 
+                ///     other two dimensions via the function <see cref="glPointSize"/>.
+                /// </remarks>
                 OpenGLAPI Point3D& Depth(float z)                             override;
+                /// <summary> Sets the size of the point along the world space y-axis. </summary>
+                /// <remarks>
+                ///     Currently, points in OpenGL may only have a uniform size in all three spatial dimensions, unless 
+                ///     custom-written shaders are available to produce different effects. Thus, attempting to set the 
+                ///     scale of a point along any world-space axis will result in that value being copied over to the 
+                ///     other two dimensions via the function <see cref="glPointSize"/>.
+                /// </remarks>
                 OpenGLAPI Point3D& Height(float y)                            override;
+                /// <summary> Sets the size of the point along the world space x-axis. </summary>
+                /// <remarks>
+                ///     Currently, points in OpenGL may only have a uniform size in all three spatial dimensions, unless 
+                ///     custom-written shaders are available to produce different effects. Thus, attempting to set the 
+                ///     scale of a point along any world-space axis will result in that value being copied over to the 
+                ///     other two dimensions via the function <see cref="glPointSize"/>.
+                /// </remarks>
                 OpenGLAPI Point3D& Width(float x)                             override;
 
+                /// <summary> Sets the size of the point in 3D world space. </summary>
+                /// <remarks>
+                ///     Currently, points in OpenGL may only have a uniform size in all three spatial dimensions, unless 
+                ///     custom-written shaders are available to produce different effects. Thus, attempting to set the 
+                ///     scale of a point along any world-space axis will result in that value being copied over to the 
+                ///     other two dimensions via the function <see cref="glPointSize"/>.
+                /// </remarks>
                 OpenGLAPI Point3D& Scale(const Vector3& value)                override;
+                /// <summary> Sets the size of the point in 3D world space. </summary>
+                /// <remarks>
+                ///     Currently, points in OpenGL may only have a uniform size in all three spatial dimensions, unless 
+                ///     custom-written shaders are available to produce different effects. Thus, attempting to set the 
+                ///     scale of a point along any world-space axis will result in that value being copied over to the 
+                ///     other two dimensions via the function <see cref="glPointSize"/>.
+                /// </remarks>
                 OpenGLAPI Point3D& Scale(float x, float y, float z = 1.0f)    override;
 
 
 
                 /** CONSTRUCTOR **/
+                /// <summary> Constructs a new white-colored point at the origin in 3D space. </summary>
 		        Point3D() : Entity3D(Color4::White, VertexTopologies::Points, Geometry::Point()) { }
         };
     }
