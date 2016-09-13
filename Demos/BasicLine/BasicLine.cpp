168d8c2bd872335cf7328242b6339193e43b2cd9
JoshGrooms <josh.grooms@gmail.com>
2016-09-13 07:46:13 -0700

    Created a new demo of basic line rendering.

---
+++ Demos/BasicLine/BasicLine.cpp
@@ -0,0 +1,73 @@
+#include "BasicRenderer.h"
+#include "GPU.h"
+#include "Window3D.h"
+#include "Geometry/Geometry.h"
+#include "Geometry/Line3D.h"
+#include "Geometry/Scene3D.h"
+#include "Pipelines/ShaderPipeline.h"
+
+using namespace Renderers;
+using namespace Cyclone::OpenGL;
+
+
+
+class Program : public BasicRenderer
+{
+    public:
+        Program() :
+            BasicRenderer("Basic Line")
+        {
+            Initialize();
+            glEnable(GL_LINE_SMOOTH);
+        }
+
+    protected:
+        Line3D Line;
+
+        void CreateSceneResources() override
+        {
+            BasicRenderer::CreateSceneResources();
+            Line.Scale(100, 100, 100)
+                .Color(Color4(0.0f, 0.75f, 1.0f))
+                .Position(Vector3(RenderWindow->ClientArea().Center(), 50))
+                .Pitch(90)
+                .Roll(90);
+
+            RenderScene->Add(Line);
+        }
+
+        void CreateSizedResources() override
+        {
+            BasicRenderer::CreateSizedResources();
+            Line.Position(Vector3(RenderWindow->ClientArea().Center(), 50));
+        }
+
+        void CreateShaderPipeline() override
+        {
+            RenderPipeline = new ShaderPipeline("../Renderers/Shaders/BlinnPhong.vsl", "../Renderers/Shaders/BlinnPhong.psl");
+            Renderer->RenderPipeline(RenderPipeline);
+        }
+
+        void UpdateScene() override
+        {
+            Color4 color
+            (
+                0.5f * sinf(Line.Pitch()) + 0.5f,
+                0.25f * cosf(Line.Yaw()) + 0.75f,
+                0.125f * sinf(Line.Roll()) + 0.875
+            );
+
+            Line.Color(color).Rotate(0.01f);
+            RenderScene->Update(Line);
+            BasicRenderer::UpdateScene();
+        }
+};
+
+
+
+int main(int nargs, char** args)
+{
+    Program app;
+    app.Execute();
+    return 0;
+}
