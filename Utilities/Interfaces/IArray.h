1eb152d4a8bca519e5e906ab00e2efe22f5fc61e
JoshGrooms <josh.grooms@gmail.com>
2016-09-17 09:45:50 -0700

    Filled in some class and method documentation.

--- Utilities/Interfaces/IArray.h
+++ Utilities/Interfaces/IArray.h
@@ -11,10 +11,12 @@ namespace Cyclone
 {
     namespace Utilities
     {
-        template<typename T>
-        struct IArray
+        /// <summary> An interface used to designate a collection as having array-like characteristics. </summary>
+        /// <typeparam name="T"> The type name of the data elements held by a collection. </typeparam>
+        template<typename T> struct IArray
         {
             public:
+                /** PROPERTIES **/
                 /// <summary> Gets the total number of elements present in an array-like collection. </summary>
                 virtual uint Count()        const = 0;
                 /// <summary> Gets whether an array-like collection is currently empty. </summary>
@@ -22,10 +24,13 @@ namespace Cyclone
                 /// <summary> Gets the number of dimensions of an array-like collection. </summary>
                 virtual uint Rank()         const = 0;
 
-
+                /** DESTRUCTOR **/
+                /// <summary> Destroys any resources that are specially allocated by a collection. </summary>
                 virtual ~IArray() { }
-                
+
+                /** OPERATORS**/
                 /// <summary> Performs linear indexing of the elements of an array-like collection. </summary>
+                /// <returns> A constant reference to the data element that resides at the inputted array index. </returns>
                 virtual const T& operator ()(uint idx) const = 0;
         };
 
