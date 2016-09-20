1002298d41208de1e1af290ce92cd57cb7be12d7
JoshGrooms <josh.grooms@gmail.com>
2016-09-20 12:54:00 -0700

    Simplified the demo by adding SVG components to 3D scenes.

--- Demos/PathRendering/PathRendering.cpp
+++ Demos/PathRendering/PathRendering.cpp
@@ -10,6 +10,7 @@
 #include "Window3D.h"
 
 #include "Buffers/FrameBuffer.h"
+#include "Geometry/Scene3D.h"
 #include "Geometry/Path2D.h"
 #include "Text/Text2D.h"
 #include "Imaging/Color4.h"
@@ -32,47 +33,40 @@ class Program : public BasicRenderer
     public:
 
         Program() : BasicRenderer("NVIDIA Path Rendering"),
-            FBO(nullptr),
-            Text(nullptr)
+            FBO(nullptr)
         {
-            ClearColor = Color4::Gray;
-
             Initialize();
         }
         ~Program()
         {
-            if (Text) { delete Text; }
             if (FBO) { delete FBO; }
         }
 
-        void Execute() override
-        {
-            while (CanContinue())
-            {
-                RenderWindow->ProcessEvent();
-                Renderer->Clear(ClearColor);
-
-                UpdateScene();
-                Renderer->Execute();
-                Renderer->Present();
-            }
-        }
 
     protected:
 
         FrameBuffer*    FBO;
         Path2D          Path;
-        Text2D*         Text;
+        Text2D          Text;
 
         void CreateSceneResources() override
         {
+            BasicRenderer::CreateSceneResources();
+
+            nvMatrixLoadIdentity(GL_PROJECTION);
+            nvMatrixOrtho(GL_PROJECTION, 0, 1024, 0, 960, -1, 1);
+
             string svgString = "M100, 180 L40, 10 L190, 120 L10, 120 L160, 10 z";
             Path
                 .FillColor(Color4::Blue)
                 .Path(svgString)
                 .StrokeWidth(6.5f);
 
-            Text = new Text2D("Testing!");
+            Text
+                .Text("Testing!");
+
+            RenderScene->Add(Path);
+            RenderScene->Add(Text);
         }
         void CreateShaderPipeline() override
         {
@@ -84,40 +78,6 @@ class Program : public BasicRenderer
             FBO = new FrameBuffer(RenderWindow->ClientArea().Scale(), TextureFormats::Byte4, TextureFormats::DepthStencil);
             Renderer->RenderTarget(FBO);
         }
-
-        void UpdateScene() override
-        {
-            glStencilMask(~0);
-            glEnable(GL_STENCIL_TEST);
-
-            nvIdentityMatrix(GL_PROJECTION);
-            nvIdentityMatrix(GL_MODELVIEW);
-            nvOrthoMatrix(GL_PROJECTION, 0, 1024, 0, 960, -1, 1);
-
-            nvStencilFillPath(Path.ID(), Path.FillMode(), 0x1F);
-            
-            glStencilFunc(GL_NOTEQUAL, 0, 0xFF);
-            glStencilOp(GL_KEEP, GL_KEEP, GL_ZERO);
-
-            //nvCoverFillPath(Path.ID(), Path.CoverMode());
-
-            nvOrthoMatrix(GL_PROJECTION, -10, 20, -10, 20, -1, 1);
-            Text->Render();
-            //nvOrthoMatrix(GL_PROJECTION, 0, 1024, 0, 960, -1, 1);
-
-            nvCoverFillPath(Path.ID(), Path.CoverMode());
-            
-            BasicRenderer::UpdateScene();
-        }
-
-        void Initialize() override
-        {
-            CreateRenderingWindow();
-            CreateShaderPipeline();
-            CreateSizedResources();
-            CreateSceneResources();
-        }
-
         
 };
 
