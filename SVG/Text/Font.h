cfaa260c7ffd361320c83594bbb039241fb57c35
JoshGrooms <josh.grooms@gmail.com>
2016-09-20 17:10:55 -0700

    Drafted some documentation for properties and methods.

--- SVG/Text/Font.h
+++ SVG/Text/Font.h
@@ -16,19 +16,28 @@ namespace Cyclone
         {
             public:
 
+                /** PROPERTIES **/
+                /// <summary> Gets the name of the typeface that the font object represents. </summary>
                 const string& Name()    const { return _name; }
+                /// <summary> Gets an enumerator representing the styling that is applied to the font when rendered. </summary>
                 FontStyles Style()      const { return _style; }
 
 
 
+                /** CONSTRUCTOR **/
+                /// <summary> Constructs a new object containing the entire collection of characters that make up a specific font family. </summary>
+                /// <param name="name"> The name of the system font to be loaded. </param>
                 SVGAPI Font(const string& name);
 
 
-                SVGAPI void Render() const override { }
 
+                /** UTILITIES **/
+                /// <summary> An empty implementation that prevents frivolously rendering an entire character collection. </summary>
+                SVGAPI void Render() const override { }
 
             private:
 
+                /** PROPERTY DATA **/
                 string      _name;
                 FontStyles  _style;
 
