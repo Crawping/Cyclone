7b288552d0fbfaeee3ee0f8ae66b8d8d8bd9d830
JoshGrooms <josh.grooms@gmail.com>
2016-09-22 14:47:51 -0700

    Forced implement a quick workaround for 'PathCommands' enumerators.
    These values are of unsigned byte types and are meant to be placed
    into arrays read by OpenGL and the GPU, meaning my usual approach
    of just converting everything to integers fails here. This probably
    means I need to make 'Enumerator' a templated class, but the number
    of those that are already present in this library is becoming
    alarmingly high. I need to do some thinking about what needs to be
    done with all of this.

--- SVG/EnumerationsSVG.h
+++ SVG/EnumerationsSVG.h
@@ -14,8 +14,8 @@ struct CharacterEncodings : public Enumerator
 {
     enum Encodings
     {
-        UTF8                            = 0x909A,
-        UTF16                           = 0x909B,
+        UTF8                                    = 0x909A,
+        UTF16                                   = 0x909B,
     };
 };
 
@@ -23,9 +23,9 @@ struct CoverModes : public Enumerator
 {
     enum Modes
     {
-        BoundingBox                     = 0x908D,
-        BoundingBoxOfBoundingBoxes      = 0x909C,
-        ConvexHull                      = 0x908B,
+        BoundingBox                             = 0x908D,
+        BoundingBoxOfBoundingBoxes              = 0x909C,
+        ConvexHull                              = 0x908B,
     };
 
     constexpr CoverModes(enum Modes m = BoundingBox) : Enumerator(m) { }
@@ -35,8 +35,8 @@ struct FillModes : public Enumerator
 {
     enum Modes
     {
-        CountUp                         = 0x9088,
-        CountDown                       = 0x9089,
+        CountUp                                 = 0x9088,
+        CountDown                               = 0x9089,
     };
 
     constexpr FillModes(enum Modes m = CountUp) : Enumerator(m) { }
@@ -46,9 +46,9 @@ struct FontStyles : public Enumerator
 {
     enum Styles
     {
-        None                            = 0,
-        Bold                            = 0x01,
-        Italic                          = 0x02,
+        None                                    = 0,
+        Bold                                    = 0x01,
+        Italic                                  = 0x02,
     };
 
     constexpr FontStyles(enum Styles s = None) : Enumerator(s) { }
@@ -58,11 +58,11 @@ struct GenerationModes : public Enumerator
 {
     enum Modes
     {
-        None                            = 0,
-        Constant                        = 0x8576,
-        EyeLinear                       = 0x2400,
-        ObjectLinear                    = 0x2401,
-        ObjectBoundingBox               = 0x908A,
+        None                                    = 0,
+        Constant                                = 0x8576,
+        EyeLinear                               = 0x2400,
+        ObjectLinear                            = 0x2401,
+        ObjectBoundingBox                       = 0x908A,
     };
 };
 
@@ -70,80 +70,92 @@ struct JoinStyles : public Enumerator
 {
     enum Styles
     {
-        None                            = 0,
-        Bevel                           = 0x90A6,
-        MiterRevert                     = 0x90A7,
-        MiterTruncate                   = 0x90A8,
-        Round                           = 0x90A4,
+        None                                    = 0,
+        Bevel                                   = 0x90A6,
+        MiterRevert                             = 0x90A7,
+        MiterTruncate                           = 0x90A8,
+        Round                                   = 0x90A4,
     };
 
     constexpr JoinStyles(enum Styles s = None) : Enumerator(s) { }
 };
 
-struct PathCommands : public Enumerator
+struct PathCommands //: public Enumerator
 {
-    enum Commands
-    {
-        Arc                                     = 0xFE,
-        CircularArcCCW                          = 0xF8,
-        CircularArcCW                           = 0xFA,
-        CircularTangentArc                      = 0xFC,
-        Close                                   = 0x00,
-        ConicCurve                              = 0x1A,
-        CubicCurve                              = 0x0C,
+    public:
+        enum Commands : ubyte
+        {
+            Arc                                     = 0xFE,
+            CircularArcCCW                          = 0xF8,
+            CircularArcCW                           = 0xFA,
+            CircularTangentArc                      = 0xFC,
+            Close                                   = 0x00,
+            ConicCurve                              = 0x1A,
+            CubicCurve                              = 0x0C,
 
-        DuplicateFirstCubicCurve                = 0xF2,
-        DuplicateLastCubicCurve                 = 0xF4,
+            DuplicateFirstCubicCurve                = 0xF2,
+            DuplicateLastCubicCurve                 = 0xF4,
 
-        HorizontalLine                          = 0x06,
-        LargeArcCCW                             = 0x16,
-        LargeArcCW                              = 0x18,
-        Line                                    = 0x04,
-        Move                                    = 0x02,
-        QuadraticCurve                          = 0x0A,
+            HorizontalLine                          = 0x06,
+            LargeArcCCW                             = 0x16,
+            LargeArcCW                              = 0x18,
+            Line                                    = 0x04,
+            Move                                    = 0x02,
+            QuadraticCurve                          = 0x0A,
 
-        Rectangle                               = 0xF6,
-        RelativeArc                             = 0xFF,
-        RelativeConicCurve                      = 0x1B,
-        RelativeCubicCurve                      = 0x0D,
-        RelativeHorizontalLine                  = 0x07,
-        RelativeLargeArcCCW                     = 0x17,
-        RelativeLargeArcCW                      = 0x19,
-        RelativeLine                            = 0x05,
-        RelativeMove                            = 0x03,
-        RelativeQuadraticCurve                  = 0x0B,
-        RelativeRectangle                       = 0xF7,
-        RelativeRoundedRectangle                = 0xE9,
-        RelativeRoundedRectangle2               = 0xEB,
-        RelativeRoundedRectangle4               = 0xED,
-        RelativeRoundedRectangle8               = 0xEF,
-        RelativeSmallArcCCW                     = 0x13,
-        RelativeSmallArcCW                      = 0x15,
-        RelativeSmoothQuadraticCurve            = 0x0F,
-        RelativeSmoothCubicCurve                = 0x11,
-        RelativeVerticalLine                    = 0x09,
-        Restart                                 = 0xF0,
+            Rectangle                               = 0xF6,
+            RelativeArc                             = 0xFF,
+            RelativeConicCurve                      = 0x1B,
+            RelativeCubicCurve                      = 0x0D,
+            RelativeHorizontalLine                  = 0x07,
+            RelativeLargeArcCCW                     = 0x17,
+            RelativeLargeArcCW                      = 0x19,
+            RelativeLine                            = 0x05,
+            RelativeMove                            = 0x03,
+            RelativeQuadraticCurve                  = 0x0B,
+            RelativeRectangle                       = 0xF7,
+            RelativeRoundedRectangle                = 0xE9,
+            RelativeRoundedRectangle2               = 0xEB,
+            RelativeRoundedRectangle4               = 0xED,
+            RelativeRoundedRectangle8               = 0xEF,
+            RelativeSmallArcCCW                     = 0x13,
+            RelativeSmallArcCW                      = 0x15,
+            RelativeSmoothQuadraticCurve            = 0x0F,
+            RelativeSmoothCubicCurve                = 0x11,
+            RelativeVerticalLine                    = 0x09,
+            Restart                                 = 0xF0,
 
-        RoundedRectangle                        = 0xE8,
-        RoundedRectangle2                       = 0xEA,
-        RoundedRectangle4                       = 0xEC,
-        RoundedRectangle8                       = 0xEE,
+            RoundedRectangle                        = 0xE8,
+            RoundedRectangle2                       = 0xEA,
+            RoundedRectangle4                       = 0xEC,
+            RoundedRectangle8                       = 0xEE,
 
-        SmallArcCCW                             = 0x12,
-        SmallArcCW                              = 0x14,
-        SmoothQuadraticCurve                    = 0x0E,
-        SmoothCubicCurve                        = 0x10,
+            SmallArcCCW                             = 0x12,
+            SmallArcCW                              = 0x14,
+            SmoothQuadraticCurve                    = 0x0E,
+            SmoothCubicCurve                        = 0x10,
 
-        VerticalLine                            = 0x08,
-    };
+            VerticalLine                            = 0x08,
+        };
+
+        constexpr PathCommands(enum Commands c = Move) :
+            Value(c)
+        {
+
+        }
+
+        constexpr operator ubyte() const { return Value; }
+
+    private:
+        ubyte Value;
 };
 
 struct PathFormats : public Enumerator
 {
     enum Formats
     {
-        SVG                             = 0x9070,
-        PS                              = 0x9071,
+        SVG                                     = 0x9070,
+        PS                                      = 0x9071,
     };
 
     constexpr PathFormats(enum Formats f = SVG) : Enumerator(f) { }
@@ -153,28 +165,28 @@ struct PathParameters : public Enumerator
 {
     enum Parameters
     {
-        ClientLength                    = 0x907F,
-        CommandCount                    = 0x909D,
-        ComputedLength                  = 0x90A0,
-        CoordinateCount                 = 0x909E,
-        DashArrayCount                  = 0x909F,
-        DashCaps                        = 0x907B,
-        DashOffset                      = 0x907E,
-        DashOffsetReset                 = 0x90B4,
-        EndCaps                         = 0x9076,
-        FillCoverMode                   = 0x9082,
-        FillMask                        = 0x9081,
-        FillMode                        = 0x9080,
-        InitialDashCap                  = 0x907C,
-        InitialEndCap                   = 0x9077,
-        JoinStyle                       = 0x9079,
-        MiterLimit                      = 0x907A,
-        StrokeCoverMode                 = 0x9083,
-        StrokeMask                      = 0x9084,
-        StrokeBound                     = 0x9086,
-        StrokeWidth                     = 0x9075,
-        TerminalDashCap                 = 0x907D,
-        TerminalEndCap                  = 0x9078,
+        ClientLength                            = 0x907F,
+        CommandCount                            = 0x909D,
+        ComputedLength                          = 0x90A0,
+        CoordinateCount                         = 0x909E,
+        DashArrayCount                          = 0x909F,
+        DashCaps                                = 0x907B,
+        DashOffset                              = 0x907E,
+        DashOffsetReset                         = 0x90B4,
+        EndCaps                                 = 0x9076,
+        FillCoverMode                           = 0x9082,
+        FillMask                                = 0x9081,
+        FillMode                                = 0x9080,
+        InitialDashCap                          = 0x907C,
+        InitialEndCap                           = 0x9077,
+        JoinStyle                               = 0x9079,
+        MiterLimit                              = 0x907A,
+        StrokeCoverMode                         = 0x9083,
+        StrokeMask                              = 0x9084,
+        StrokeBound                             = 0x9086,
+        StrokeWidth                             = 0x9075,
+        TerminalDashCap                         = 0x907D,
+        TerminalEndCap                          = 0x9078,
     };
 
     constexpr PathParameters(enum Parameters p) : Enumerator(p) { }
@@ -184,8 +196,8 @@ struct TransformMatrices : public Enumerator
 {
     enum Matrices
     {
-        ModelView                       = 0x1700,
-        Projection                      = 0x1701,
+        ModelView                               = 0x1700,
+        Projection                              = 0x1701,
     };
 };
 
@@ -193,14 +205,14 @@ struct TransformTypes : public Enumerator
 {
     enum Types
     {
-        None                            = 0,
-        TranslateX                      = 0x908E,
-        TranslateY                      = 0x908F,
-        Translate2D                     = 0x9090,
-        Translate3D                     = 0x9091,
-        Affine2D                        = 0x9092,
-        Affine3D                        = 0x9094,
-        TransposeAffine2D               = 0x9096,
-        TransposeAffine3D               = 0x9098,
+        None                                    = 0,
+        TranslateX                              = 0x908E,
+        TranslateY                              = 0x908F,
+        Translate2D                             = 0x9090,
+        Translate3D                             = 0x9091,
+        Affine2D                                = 0x9092,
+        Affine3D                                = 0x9094,
+        TransposeAffine2D                       = 0x9096,
+        TransposeAffine3D                       = 0x9098,
     };
 };
