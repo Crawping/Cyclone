74cbf91d72dfa2c039cc2746fa9946debc059b11
JoshGrooms <josh.grooms@gmail.com>
2016-09-23 09:58:07 -0700

    Began writing a static library of common SVG geometry.

---
+++ SVG/Geometry/Geometry2D.cpp
@@ -0,0 +1,51 @@
+#include "Geometry/Geometry2D.h"
+
+
+
+namespace Cyclone
+{
+    namespace SVG
+    {
+
+        Vector<ControlPoint2D> Geometry2D::Line()
+        {
+            return
+            {
+                { PathCommands::Move,   { -0.5, 0 } },
+                { PathCommands::Line,   {  0.5, 0 } },
+                { PathCommands::Close,  { } },
+            };
+        }
+
+        Vector<ControlPoint2D> Geometry2D::Rectangle()
+        {
+            return 
+            {
+                { PathCommands::Rectangle,  { 0, 0, 100, 100 } },
+                { PathCommands::Close,      { } },
+            };
+        }
+
+        Vector<ControlPoint2D> Geometry2D::RoundedRectangle()
+        {
+            return
+            {
+                { PathCommands::RoundedRectangle,   { 0, 0, 100, 100, 12 } },
+                { PathCommands::Close,              { } },
+            };
+        }
+
+        Vector<ControlPoint2D> Geometry2D::Star()
+        {
+            return
+            {
+                { PathCommands::Move,   { 100, 180 } },
+                { PathCommands::Line,   { 40, 10 } },
+                { PathCommands::Line,   { 190, 120 } },
+                { PathCommands::Line,   { 10, 120 } },
+                { PathCommands::Line,   { 160, 10 } },
+                { PathCommands::Close,  { } },
+            };
+        }
+    }
+}
---
+++ SVG/Geometry/Geometry2D.h
@@ -0,0 +1,34 @@
+/* CHANGELOG
+ * Written by Josh Grooms on 20160923
+ */
+
+#pragma once
+#include "Collections/Vector.h"
+#include "Geometry/ControlPoint2D.h"
+#include "SVGAPI.h"
+
+
+
+
+namespace Cyclone
+{
+    namespace SVG
+    {
+        namespace Geometry2D
+        {
+
+            //SVGAPI Vector<ControlPoint2D> Circle();
+
+            SVGAPI Vector<ControlPoint2D> Line();
+
+            SVGAPI Vector<ControlPoint2D> Rectangle();
+
+            SVGAPI Vector<ControlPoint2D> RoundedRectangle();
+            
+            SVGAPI Vector<ControlPoint2D> Star();
+
+            SVGAPI Vector<ControlPoint2D> Triangle();
+
+        }
+    }
+}
