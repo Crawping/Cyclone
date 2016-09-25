1acebb75a8bc9d05caa08fd4017376698009459e
JoshGrooms <josh.grooms@gmail.com>
2016-09-25 09:52:06 -0700

    Implemented a camera transformation class.

---
+++ Utilities/Spatial/Camera.cpp
@@ -0,0 +1,105 @@
+#include "Utilities.h"
+#include "Math/Constants.h"
+#include "Math/Matrix4x4.h"
+#include "Spatial/Camera.h"
+#include "Spatial/Transform.h"
+
+
+
+namespace Cyclone
+{
+    namespace Utilities
+    {
+
+        /** PROPERTIES **/
+        Vector3 Camera::Forward() const
+        {
+            return Vector3(State(2, 0), State(2, 1), State(2, 2)).Normalize();
+        }
+
+        Vector3 Camera::Right() const
+        {
+            return Vector3(State(0, 0), State(0, 1), State(0, 2)).Normalize();
+        }
+
+        Camera& Camera::Orientation(const Vector3& value)
+        {
+            _needsUpdate = true;
+
+            _orientation.X = clamp(value.X, -(float)Constants::HalfPi + 0.001f, (float)Constants::HalfPi - 0.001f);
+            _orientation.Y = wrap(value.Y, 0.0f, (float)Constants::TwoPi - 0.001f);
+            _orientation.Z = wrap(value.Z, 0.0f, (float)Constants::TwoPi - 0.001f);
+            
+            return *this;
+        }
+        Camera& Camera::Position(const Vector3& value)
+        {
+            _needsUpdate = true;
+            _position = value;
+            return *this;
+        }
+        Camera& Camera::Scale(const Vector3& value)
+        {
+            _needsUpdate = true;
+            _scale = value;
+            return *this;
+        }
+
+
+        /** CONSTRUCTOR **/
+        Camera::Camera() :
+            _needsUpdate(false),
+            _scale(1.0f)
+        {
+
+        }
+
+
+
+        /** UTILITIES **/
+        Camera& Camera::Rotate(const Vector3& r)
+        {
+            return Orientation(Pitch() + r.X, Yaw() + r.Y, Roll() + r.Z);
+        }
+        Camera& Camera::Translate(const Vector3& t)
+        {
+            return Position(X() + t.X, Y() + t.Y, Z() + t.Z);
+        }
+
+
+
+        /** PRIVATE UTILITIES **/
+        /// <summary> Calculates the value for each element of the state matrix. </summary>
+        /// <remarks>
+        ///     Scale * Pitch * Yaw * Roll * Translation
+        /// </remarks>
+        void Camera::Update() const
+        {
+            if (!_needsUpdate) { return; }
+
+            float cr, cp, cy, sr, sp, sy, w, h, d, x, y, z;
+            w = _scale.X; h = _scale.Y; d = _scale.Z;
+            x = -_position.X; y = -_position.Y; z = -_position.Z;
+            cr = (float)cos(_orientation.Z); cp = (float)cos(_orientation.X); cy = (float)cos(_orientation.Y);
+            sr = (float)sin(_orientation.Z); sp = (float)sin(_orientation.X); sy = (float)sin(_orientation.Y);
+
+            State(0)  = (cr * cy * w);
+            State(1)  = (cp * h * sr) + (cr * h * sp * sy);
+            State(2)  = (d * sp * sr) - (cp * cr * d * sy);
+                      
+            State(4)  = -(cy * sr * w);
+            State(5)  = (cp * cr * h) - (h * sp * sr * sy);
+            State(6)  = (cr * d * sp) + (cp * d * sr * sy);
+                      
+            State(8)  = sy * w;
+            State(9)  = -(cy * h * sp);
+            State(10) = cp * cy * d;
+
+            State(12) = (sy * w * z) + (cr * cy * w * x) - (cy * sr * w * y);
+            State(13) = ( x * ((cp*h*sr) + (cr*h*sp*sy)) ) + ( y * ((cp*cr*h) - (h*sp*sr*sy)) ) - (cy*h*sp*z);
+            State(14) = ( x * ((d*sp*sr) - (cp*cr*d*sy)) ) + ( y * ((cr*d*sp) + (cp*d*sr*sy)) ) + (cp*cy*d*z);
+
+            _needsUpdate = false;
+        }
+    }
+}
---
+++ Utilities/Spatial/Camera.h
@@ -0,0 +1,82 @@
+/* CHANGELOG
+ * Written by Josh Grooms on 20160923
+ */
+
+#pragma once
+#include "UtilitiesAPI.h"
+#include "Interfaces/ITransformation3D.h"
+#include "Math/Matrix4x4.h"
+
+
+
+namespace Cyclone
+{
+    namespace Utilities
+    {
+        class Camera : public ITransformation3D
+        {
+            public:
+                float X()                                                       const { return _position.X; }
+                float Y()                                                       const { return _position.Y; }
+                float Z()                                                       const { return _position.Z; }
+
+                float Pitch()                                                   const { return _orientation.X; }
+                float Roll()                                                    const { return _orientation.Z; }
+                float Yaw()                                                     const { return _orientation.Y; }
+
+                const Vector3& Orientation()                                    const override { return _orientation; }
+                const Vector3& Position()                                       const override { return _position; }
+                const Vector3& Scale()                                          const override { return _scale; }
+
+
+                Camera& X(float x)                                                       { return Position(x, Y(), Z()); }
+                Camera& Y(float y)                                                       { return Position(X(), y, Z()); }
+                Camera& Z(float z)                                                       { return Position(X(), Y(), z); }
+                                                                                         
+                Camera& Pitch(float p)                                                   { return Orientation(p, Yaw(), Roll()); }
+                Camera& Roll(float r)                                                    { return Orientation(Pitch(), Yaw(), r); }
+                Camera& Yaw(float y)                                                     { return Orientation(Pitch(), y, Roll()); }
+
+                Camera& Orientation(float p, float y, float r)                           { return Orientation(Vector3(p, y, r)); }
+                UtilitiesAPI Camera& Orientation(const Vector3& value)          override;
+                Camera& Position(float x, float y, float z)                              { return Position(Vector3(x, y, z)); }
+                UtilitiesAPI Camera& Position(const Vector3& value)             override;
+                Camera& Scale(float x, float y, float z)                                 { return Scale(Vector3(x, y, z)); }
+                UtilitiesAPI Camera& Scale(const Vector3& value)                override;
+
+                Vector3 Backward()                                              const { return -Forward(); }
+                UtilitiesAPI Vector3 Forward()                                  const;
+                Vector3 Left()                                                  const { return -Right(); }
+                UtilitiesAPI Vector3 Right()                                    const;
+                
+                
+                UtilitiesAPI Camera& Translate(const Vector3& t)                override;
+                UtilitiesAPI Camera& Rotate(const Vector3& r)                   override;
+
+
+                const Matrix4x4& ToMatrix4x4()                                  const override { Update(); return State; }
+
+
+
+
+                UtilitiesAPI Camera();
+
+
+            private:
+
+                /** PROPERTY DATA **/
+                mutable bool    _needsUpdate;
+                Vector3         _orientation;
+                Vector3         _position;
+                Vector3         _scale;
+
+
+                /** DATA **/
+                mutable Matrix4x4 State;
+
+
+                /** UTILITIES **/
+                void Update() const;
+        };
+    }
+}
