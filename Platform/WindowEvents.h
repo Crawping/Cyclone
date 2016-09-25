52b618ccbcf78dd3a6b1a3ef4db187b096470ca6
JoshGrooms <josh.grooms@gmail.com>
2016-09-25 12:17:32 -0700

    Updated some variable names and implemented prototype keyboard events.

--- Platform/WindowEvents.h
+++ Platform/WindowEvents.h
@@ -17,28 +17,32 @@ namespace Cyclone
 
     namespace Platform
     {
+        
         /// <summary> An event structure containing new pointer position data. </summary>
         /// <remarks>
-        ///     This event structure gets dispatched every time the pointer moves within the client area of a 
+        ///     This event structure gets dispatched every time the pointer moves within the client area of a
         ///     <see cref="Window3D"/> rendering window.
         /// </remarks>
         struct PointerMotionEvent
         {
-            /// <summary> The new (x, y) pixel position of the pointer relative to the upper-left corner of the window's client area. </summary>
-            Vector2 PointerPosition;
             /// <summary> The (x, y) change in position relative to where the pointer was before the motion event was triggered. </summary>
-            Vector2 PointerPositionDelta;
+            Vector2 Delta;
+            /// <summary> The new (x, y) pixel position of the pointer relative to the upper-left corner of the window's client area. </summary>
+            Vector2 Position;
         };
 
         /// <summary> An event structure containing information about pointer button clicks. </summary>
         struct PointerClickEvent
         {
             InputControls Button;
-            InputControls Chord;
+            InputControls State;
         };
 
-        
-
+        struct KeyboardEvent
+        {
+            const KeyboardKeys& Key;
+            const KeyboardKeys& State;
+        };
 
     }
 }
