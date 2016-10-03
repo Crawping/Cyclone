6d566912adcd0323a16a8b4544262eee22066e6f
JoshGrooms <josh.grooms@gmail.com>
2016-10-03 06:39:29 -0700

    Created a new demo of loading UHR images into OpenGL textures.

---
+++ Demos/ImageDisplay/ImageDisplay.cpp
@@ -0,0 +1,80 @@
+/* CHANGELOG 
+ * Written by Josh Grooms on 20161001
+ */
+
+#include "AdvancedRenderer.h"
+#include "Console.h"
+#include "EnumerationsGL.h"
+#include "GPU.h"
+#include "Window3D.h"
+
+#include "Geometry/Quad3D.h"
+#include "Geometry/Scene3D.h"
+#include "Imaging/Bitmap.h"
+#include "Pipelines/ShaderPipeline.h"
+#include "Textures/Texture2D.h"
+using namespace Renderers;
+
+
+
+class Program : public AdvancedRenderer
+{
+    public:
+
+        Program() : 
+            AdvancedRenderer(Area(0, 0, 1024, 960), "Image Display", 4),
+            Image(nullptr)
+        {
+            Initialize();
+        }
+
+        ~Program()
+        {
+            if (Image) { delete Image; }
+        }
+
+    protected:
+
+        Texture2D*  Image;
+        Quad3D      Quad;
+
+        
+        void CreateSceneResources() override
+        {
+            AdvancedRenderer::CreateSceneResources();
+            PostInfo(Renderer->Report());
+            Image = new Texture2D("../ImageDisplay/World (21600x10800).jpg");
+            //Image = new Texture2D("../ImageDisplay/Dorset Durdle (6400x4000).jpg");
+            //Image = new Texture2D("../ImageDisplay/Scottish Beach (1080x1920).jpg");
+            //Image = new Texture2D("../ImageDisplay/Solar System (12000x7000).jpg");
+            PostInfo(Renderer->Report());
+
+            Image->Sampler.EdgeWrap = WrapModes::Repeat;
+            Image->GenerateMipmap();
+            Image->Bind();
+
+            Quad
+                .Color(Color4::Blue)
+                .Position(RenderWindow->ClientArea().Center())
+                .Scale(Image->Width() / 10.0f, Image->Height() / 10.0f);
+
+            RenderScene->Add(Quad);
+
+            PostInfo(Renderer->Report());
+        }
+        void CreateShaderPipeline() override
+        {
+            RenderPipeline = new ShaderPipeline("../Renderers/Shaders/Default.vsl", "../ImageDisplay/TexturedShading.psl");
+            Renderer->RenderPipeline(RenderPipeline);
+        }
+
+};
+
+
+
+int main(int nargs, char** args)
+{
+    Program app;
+    app.Execute();
+    return 0;
+}
