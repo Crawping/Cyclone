5aa1401a14ad14805d9b0277e4a43024ad1d8de2
JoshGrooms <josh.grooms@gmail.com>
2016-10-05 07:45:30 -0700

    Drafted documentation for all interface methods.

--- Utilities/Interfaces/ITransformation3D.h
+++ Utilities/Interfaces/ITransformation3D.h
@@ -10,28 +10,46 @@ namespace Cyclone
 {
     namespace Utilities
     {
+
+        /** FORWARD DECLARATIONS**/
         struct Matrix4x4;
         struct Vector3;
 
-
+        /// <summary> An interface that represents a generalized 3D affine geometric transformation. </summary>
         class ITransformation3D
         {
             public:
-                virtual const Vector3& Orientation()    const = 0;
-                virtual const Vector3& Position()       const = 0;
-                virtual const Vector3& Scale()          const = 0;
 
-                virtual ITransformation3D& Orientation(const Vector3& value) = 0;
-                virtual ITransformation3D& Position(const Vector3& value) = 0;
-                virtual ITransformation3D& Scale(const Vector3& value) = 0;
+                /** PROPERTIES **/
+                /// <summary> Gets the (x, y, z) rotation angles of the transformation in radians. </summary>
+                virtual const Vector3& Orientation()                            const = 0;
+                /// <summary> Gets the (x, y, z) translation components of the transformation. </summary>
+                virtual const Vector3& Position()                               const = 0;
+                /// <summary> Gets the (x, y, z) scaling components of the transformation. </summary>
+                virtual const Vector3& Scale()                                  const = 0;
+
+                /// <summary> Sets the (x, y, z) rotation angles of the transformation in radians. </summary>
+                virtual ITransformation3D& Orientation(const Vector3& value)          = 0;
+                /// <summary> Sets the (x, y, z) translation components of the transformation. </summary>
+                virtual ITransformation3D& Position(const Vector3& value)             = 0;
+                /// <summary> Sets the (x, y, z) scaling components of the transformation. </summary>
+                virtual ITransformation3D& Scale(const Vector3& value)                = 0;
 
 
+
+                /** DESTRUCTOR**/
                 virtual ~ITransformation3D() { }
 
-                virtual ITransformation3D& Translate(const Vector3& translation) = 0;
-                virtual ITransformation3D& Rotate(const Vector3& rotation) = 0;
 
-                virtual const Matrix4x4& ToMatrix4x4() const = 0;
+
+                /** UTILITIES**/
+                /// <summary> Sets the (x, y, z) translation components of the transformation relative to their current values. </summary>
+                virtual ITransformation3D& Translate(const Vector3& translation)      = 0;
+                /// <summary> Sets the (x, y, z) rotation angles of the transformation relative to their current values. </summary>
+                virtual ITransformation3D& Rotate(const Vector3& rotation)            = 0;
+                /// <summary> Converts the transformation data structure into an equivalent 4x4 transformation matrix. </summary>
+                virtual const Matrix4x4& ToMatrix4x4()                          const = 0;
+
         };
     }
 }
