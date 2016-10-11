97a5d1a34ae78db22d05ee90cff5a8fcd3e2a35c
JoshGrooms <josh.grooms@gmail.com>
2016-10-11 06:25:39 -0700

    Working on implementing spatial voxel octrees.

--- OpenGL/Geometry/Space3D.h
+++ OpenGL/Geometry/Space3D.h
@@ -14,36 +14,89 @@ namespace Cyclone
 {
     namespace OpenGL
     {
-        struct SpatialNode
-        {
-            public:
-
-                SpatialNode() : Nodes(nullptr) { }
-
-            private:
-                using IRenderable = const IRenderable3D<Vertex::Standard>*;
-
-                Volume                      Bounds;
-                List<IRenderable>           Entities;
-                Vector<SpatialNode*, 8>     Nodes;
-
-        };
-
-
 
         class Space3D
         {
             public:
-                virtual const Vector3& Position()   const { return 0.0f; }
-                virtual const Vector3& Size()       const { return 0.0f; }
+                virtual const Vector3& Position()   const { return Voxel->Position(); }
+                virtual const Vector3& Size()       const { return Voxel->Size(); }
+
+
+                Space3D() :
+                    Root(nullptr),
+                    Voxel(nullptr)
+                {
+                    Root = new SpatialNode();
+                    Voxel = Root;
+                }
+
+                ~Space3D()
+                {
+                    if (Root) { delete Root; }
+                }
+
+
+
+                virtual Space3D& Insert(const IRenderable3D<Vertex::Standard>& entity)
+                {
+
+                }
+
 
 
             private:
-                
+                struct SpatialNode;
+
+                SpatialNode*    Root;
                 SpatialNode*    Voxel;
 
                 
-                
+
+                struct SpatialNode
+                {
+                    public:
+                        using IRenderable = const IRenderable3D<Vertex::Standard>*;    
+
+
+                        const Vector3& Position()   const { return _bounds.Position(); }
+                        const Vector3& Size()       const { return _bounds.Size(); }
+
+
+                        SpatialNode() : 
+                            _parent(nullptr),
+                            Nodes(nullptr) 
+                        {
+
+                        }
+                        ~SpatialNode()
+                        {
+                            for (uint a = 0; a < Nodes.Count(); a++)
+                                if (Nodes(a)) { delete Nodes(a); }                                    
+                        }
+
+
+
+                        SpatialNode*& Node(uint idx) { return Nodes(idx); }
+                        
+
+                        bool Contains(const Vector3& point) const { return _bounds.Contains(point); }
+                        bool Contains(const Volume& volume) const { return _bounds.Contains(volume); }
+
+                        SpatialNode* Insert(IRenderable entity)
+                        {
+                            //if (!Contains(entity->)
+                        }
+
+
+                    private:
+                        
+                        Volume                      _bounds;
+                        SpatialNode*                _parent;
+
+                        List<IRenderable>          Entities;
+                        Vector<SpatialNode*, 8>     Nodes;
+
+                };
 
 
         };
