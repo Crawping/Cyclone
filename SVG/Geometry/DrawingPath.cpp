f090f336d345fa80587664872958f727e5a3e77d
JoshGrooms <josh.grooms@gmail.com>
2016-10-11 06:21:15 -0700

    Began implementing a dynamic path class for drawing with SVG.

---
+++ SVG/Geometry/DrawingPath.cpp
@@ -0,0 +1,57 @@
+#include "Geometry/DrawingPath.h"
+#include "Geometry/Rectangle2D.h"
+
+
+
+namespace Cyclone
+{
+    namespace SVG
+    {
+
+        /** CONSTRUCTOR & DESTRUCTOR **/
+        DrawingPath::DrawingPath() : 
+            _pointColor(Color4::Black),
+            _pointSize(2 * Path2D::StrokeWidth())
+        {
+        
+        }
+        DrawingPath::~DrawingPath()
+        {
+            for (uint a = 0; a < Points.Count(); a++)
+                delete Points(a);
+        }
+
+
+
+        /** UTILITIES **/
+        DrawingPath& DrawingPath::Add(const ControlPoint2D& point)
+        {
+            Path2D::Add(point);
+            Vector3 ptCoords(point.Coordinates(0), point.Coordinates(1), 0.0f);
+            Rectangle2D* pt = new Rectangle2D(PointSize().X / 4.0f);
+
+            pt->JoinStyle(JoinStyles::Round)
+                .StrokeColor(_pointColor)
+                .StrokeWidth(StrokeWidth() * 1.5f)
+                .Position(ptCoords);
+
+            Points.Append(pt);
+            return *this;
+        }
+
+        void DrawingPath::Clear()
+        {
+            Path2D::Clear();
+            for (uint a = 0; a < Points.Count(); a++)
+                delete Points(a);
+        }
+
+        void DrawingPath::Render(const GPU* gpu) const
+        {
+            Path2D::Render(gpu);
+            for (uint a = 0; a < Points.Count(); a++)
+                Points(a)->Render(gpu);
+        }
+
+    }
+}
---
+++ SVG/Geometry/DrawingPath.h
@@ -0,0 +1,46 @@
+/* CHANGELOG
+ * Written by Josh Grooms on 20161008
+ */
+
+#pragma once
+#include "Geometry/Path2D.h"
+
+
+
+namespace Cyclone
+{
+    namespace SVG
+    {
+        class Rectangle2D;
+
+        class DrawingPath : public Path2D
+        {
+            public:
+
+                /** PROPERTIES **/
+                const Vector2& PointSize() const  { return _pointSize; }
+
+
+
+                /** CONSTRUCTOR & DESTRUCTOR **/
+                SVGAPI DrawingPath();
+                SVGAPI ~DrawingPath();
+
+
+
+                /** UTILITIES **/
+                SVGAPI DrawingPath& Add(const ControlPoint2D& point)            override;
+                SVGAPI void Clear()                                             override;
+                SVGAPI void Render(const GPU* gpu)                              const override;
+
+            private:
+
+                /** PROPERTY DATA **/
+                Vector2             _pointSize;
+                Color4              _pointColor;
+
+                List<Rectangle2D*>  Points;
+
+        };
+    }
+}
