d68d7d9d6cc833c3bbef51aa6f034d26cd5e4f43
JoshGrooms <josh.grooms@gmail.com>
2016-10-13 06:10:42 -0700

    Updated for compatibility with recent changes to 'Vertex' data structures.

--- Testing/Vertex.cpp
+++ Testing/Vertex.cpp
@@ -29,21 +29,17 @@ class _Vertex : public testing::Test
 TEST_F(_Vertex, DefaultConstruction)
 {
     ASSERT_EQ(_v0.Position, Vector3::Zero);
-    ASSERT_EQ(_v0.UV, Vector2::Zero);
 
-    ASSERT_EQ(sizeof(Vertex::Simple), sizeof(Vector3) + sizeof(Vector2) + sizeof(IVertex));
+    ASSERT_EQ(sizeof(Vertex::Simple), sizeof(Vector3) + sizeof(IVertex));
 }
 
 TEST_F(_Vertex, VertexLayouts)
 {
-    ASSERT_EQ(_a1.Count(), 2);
+    ASSERT_EQ(_a1.Count(), 1);
 
     ASSERT_EQ(_a1(0).Count, 3);
     ASSERT_EQ(_a1(0).Format, NumericFormats::Float);
     ASSERT_EQ(_a1(0).Offset, offsetof(Vertex::Simple, Position));
-
-    ASSERT_EQ(_a1(1).Count, 2);
-    ASSERT_EQ(_a1(1).Offset, offsetof(Vertex::Simple, UV));
 
     ASSERT_EQ(_a2.Count(), 3);
     ASSERT_EQ(_a2(0).Count, 3);
