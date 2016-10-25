aa4817b2181ba7b90bd971b43101a6be12dfee4a
JoshGrooms <josh.grooms@gmail.com>
2016-10-25 15:50:49 -0700

    Prototyping an interface for all UI controls.

--- UserInterface/Interfaces/IInterfaceControl.h
+++ UserInterface/Interfaces/IInterfaceControl.h
@@ -3,20 +3,34 @@
  */
 
 #pragma once
+#include "Event.h"
 
 
 
 namespace Cyclone
 {
+    namespace { using namespace Utilities; }
+
     namespace UI
     {
-
         class IInterfaceControl
         {
             public:
 
+                /** PROPERTIES **/
                 virtual bool IsEnabled() const = 0;
 
+
+
+                /** EVENT HANDLERS **/
+                virtual void ProcessButtonPress()   = 0;
+                virtual void ProcessButtonRelease() = 0;
+                virtual void ProcessKeyPress()      = 0;
+                virtual void ProcessKeyRelease()    = 0;
+                virtual void ProcessPointerHover()  = 0;
+
+
+                
                 virtual ~IInterfaceControl() { }
         };
     }
