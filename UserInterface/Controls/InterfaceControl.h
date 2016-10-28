3a9cfa6fbfe983d6d51f32fca14e83f2078b9d2f
JoshGrooms <josh.grooms@gmail.com>
2016-10-28 09:03:38 -0700

    Working on a solid base class for GUI controls.

--- UserInterface/Controls/InterfaceControl.h
+++ UserInterface/Controls/InterfaceControl.h
@@ -3,9 +3,11 @@
  */
 
 #pragma once
-#include "Imaging/Color4.h"
+#include "Geometry/Path2D.h"
+//#include "Imaging/Color4.h"
 #include "Interfaces/IInterfaceControl.h"
-#include "Math/Vector4.h"
+#include "Interfaces/IRenderable2D.h"
+//#include "Math/Vector4.h"
 #include "Spatial/Area.h"
 #include "Spatial/Border.h"
 
@@ -13,7 +15,11 @@
 
 namespace Cyclone
 {
-    namespace { using namespace Utilities; }
+    namespace 
+    { 
+        using namespace SVG;
+        using namespace Utilities; 
+    }
 
     namespace UI
     {
@@ -24,28 +30,59 @@ namespace Cyclone
             Color4      BorderColor;
             Border      BorderWidth;
             Border      BorderRadius;
+            Color4      FontColor;
             string      FontName;
             float       FontSize;
             uint        FontWeight;
             Color4      ForegroundColor;
+            Border      Margin;
+            Border      Padding;
         };
 
 
-        class InterfaceControl : public IInterfaceControl
+        class InterfaceControl : 
+            public virtual IInterfaceControl,
+            public virtual IRenderable2D<float>
         {
             public:
                 
-            
-                virtual bool IsEnabled()                            const { return _isEnabled; }
-
+                /** PROPERTIES **/
+                virtual bool IsEnabled()                            const override { return _isEnabled; }
+                virtual IInterfaceControl& Parent()                 const override { return *_parent; }
+                virtual VertexTopologies Topology()                 const override { return _shape->Topology(); }
 
                 virtual InterfaceControl& IsEnabled(bool value)     { _isEnabled = value; }
 
 
 
+                /** DESTRUCTOR **/
+                virtual ~InterfaceControl() { }
+
+
+
+                /** UTILITIES **/
+                virtual void Fill()     const override { _shape->Fill(); }
+                virtual void Stroke()   const override { _shape->Stroke(); }
+
+
+
+            protected:
+
+                InterfaceControl() : 
+                    _isEnabled(true),
+                    _parent(nullptr),
+                    _shape(nullptr)
+                {
+
+                }
+
+
+
             private:
                 
-                bool        _isEnabled;
+                bool                _isEnabled;
+                InterfaceControl*   _parent;
+                Path2D*             _shape;
 
 
         };
