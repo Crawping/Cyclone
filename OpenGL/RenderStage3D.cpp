142147ac39f8b08835bf1d7d62bb5d1c00e59084
JoshGrooms <josh.grooms@gmail.com>
2016-11-15 14:59:25 -0800

    Implemented a batch 'Settings' property.

--- OpenGL/RenderStage3D.cpp
+++ OpenGL/RenderStage3D.cpp
@@ -1,4 +1,3 @@
-#include "GraphicsSettings.h"
 #include "RenderStage3D.h"
 #include "Buffers/GraphicsBuffer.h"
 
@@ -12,49 +11,67 @@ namespace Cyclone
         /** PROPERTIES **/
         RenderStage3D& RenderStage3D::IsBlendEnabled(bool value) 
         { 
-            _settings->IsBlendEnabled = value; 
+            _settings.IsBlendEnabled = value; 
             return *this;
         }
         RenderStage3D& RenderStage3D::IsDepthTestEnabled(bool value) 
         { 
-            _settings->IsDepthTestEnabled = value; 
+            _settings.IsDepthTestEnabled = value; 
             return *this;
         }
         RenderStage3D& RenderStage3D::IsStencilTestEnabled(bool value)
         { 
-            _settings->IsStencilTestEnabled = value; 
+            _settings.IsStencilTestEnabled = value; 
             return *this;
         }
         RenderStage3D& RenderStage3D::CullingMode(CullingModes value)
         {
-            _settings->CullingMode = value;
+            _settings.CullingMode = value;
             return *this;
         }
         RenderStage3D& RenderStage3D::Pipeline(GraphicsPipeline* value)
         { 
-            _settings->Pipeline = value;
+            _settings.Pipeline = value;
             return *this;
         }
         RenderStage3D& RenderStage3D::Projection(ITransformation3D* value) 
         { 
-            _settings->Projection = value;
+            _settings.Projection = value;
             return *this;
+        }
+        RenderStage3D& RenderStage3D::Settings(const GraphicsSettings& value)
+        {
+            return this->
+                 IsBlendEnabled(value.IsBlendEnabled)
+                .IsDepthTestEnabled(value.IsDepthTestEnabled)
+                .IsStencilTestEnabled(value.IsStencilTestEnabled)
+                .CullingMode(value.CullingMode)
+                .Pipeline(value.Pipeline)
+                .Projection(value.Projection)
+                .Target(value.Target)
+                .View(value.View);
         }
         RenderStage3D& RenderStage3D::Target(FrameBuffer* value)
         { 
-            _settings->Target = value; 
+            _settings.Target = value; 
             return *this;
         }
         RenderStage3D& RenderStage3D::View(ITransformation3D* value) 
         { 
-            _settings->View = value;
+            _settings.View = value;
             return *this;
         }
 
 
 
         /** CONSTRUCTOR **/
-        RenderStage3D::RenderStage3D(VertexTopologies topology, const IGraphicsBuffer* data, GraphicsSettings* settings) :
+        RenderStage3D::RenderStage3D(VertexTopologies topology, const IGraphicsBuffer* data) :
+            _data(data),
+            _topology(topology)
+        {
+
+        }
+        RenderStage3D::RenderStage3D(VertexTopologies topology, const IGraphicsBuffer* data, const GraphicsSettings& settings) :
             _data(data),
             _settings(settings),
             _topology(topology)
--- OpenGL/RenderStage3D.h
+++ OpenGL/RenderStage3D.h
@@ -4,6 +4,7 @@
 
 #pragma once
 #include "EnumerationsGL.h"
+#include "GraphicsSettings.h"
 #include "Interfaces/IRenderStage.h"
 #include <map>
 
@@ -18,7 +19,6 @@ namespace Cyclone
         class IGraphicsBuffer;
         class FrameBuffer;
         class GraphicsPipeline;
-        struct GraphicsSettings;
 
         struct RenderStage3D : public virtual IRenderStage
         {
@@ -26,7 +26,7 @@ namespace Cyclone
 
                 /** PROPERTIES **/
                 const IGraphicsBuffer* Data()                               const override { return _data; }
-                const GraphicsSettings& Settings()                          const override { return *_settings; }
+                const GraphicsSettings& Settings()                          const override { return _settings; }
                 VertexTopologies Topology()                                 const override { return _topology; }
 
                 OpenGLAPI RenderStage3D& IsBlendEnabled(bool value);
@@ -35,13 +35,15 @@ namespace Cyclone
                 OpenGLAPI RenderStage3D& CullingMode(CullingModes value);
                 OpenGLAPI RenderStage3D& Pipeline(GraphicsPipeline* value);
                 OpenGLAPI RenderStage3D& Projection(ITransformation3D* value);
+                OpenGLAPI RenderStage3D& Settings(const GraphicsSettings& value);
                 OpenGLAPI RenderStage3D& Target(FrameBuffer* value);
                 OpenGLAPI RenderStage3D& View(ITransformation3D* value);
 
 
 
                 /** CONSTRUCTOR **/
-                OpenGLAPI RenderStage3D(VertexTopologies topology, const IGraphicsBuffer* data, GraphicsSettings* settings);
+                OpenGLAPI RenderStage3D(VertexTopologies topology, const IGraphicsBuffer* data);
+                OpenGLAPI RenderStage3D(VertexTopologies topology, const IGraphicsBuffer* data, const GraphicsSettings& settings);
 
 
 
@@ -51,7 +53,7 @@ namespace Cyclone
             private:
 
                 const IGraphicsBuffer*  _data;
-                GraphicsSettings*       _settings;
+                GraphicsSettings        _settings;
                 VertexTopologies        _topology;
 
         };
@@ -60,7 +62,7 @@ namespace Cyclone
         {
             public:
 
-                IndexedRenderStage3D(VertexTopologies topology, const IGraphicsBuffer* data, GraphicsSettings* settings) : 
+                IndexedRenderStage3D(VertexTopologies topology, const IGraphicsBuffer* data, const GraphicsSettings& settings) : 
                     RenderStage3D(topology, data, settings)
                 {
 
