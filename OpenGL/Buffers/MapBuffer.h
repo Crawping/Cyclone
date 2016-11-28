633bbe990748bf76950967361cb3b3f422349311
JoshGrooms <josh.grooms@gmail.com>
2016-11-28 05:54:12 -0800

    Updated to use the new 'BST' class for data storage instead of 'std::map'.

--- OpenGL/Buffers/MapBuffer.h
+++ OpenGL/Buffers/MapBuffer.h
@@ -3,8 +3,10 @@
  */
 
 #pragma once
+#include "Collections/BST.h"
+#include "Collections/List.h"
 #include "Buffers/GraphicsBuffer.h"
-#include <map>
+//#include <map>
 
 
 
@@ -20,31 +22,39 @@ namespace Cyclone
 
                 /** PROPERTIES **/
                 /// <summary> Gets the number of individual elements stored within this buffer. </summary>
-                virtual uint Count()            const override { return Data.size(); }
+                virtual uint Count()            const override { return _data.Count(); }
                 /// <summary> Gets the number of bytes occupied by one individual element of this buffer. </summary>
                 virtual ulong Stride()          const override { return sizeof(U); }
 
+                virtual List<T> Keys()          const { return _data.Keys(); }
+
+                virtual List<U> Values()        const { return _data.Values(); }
 
 
                 /** UTILITIES **/
+
+                virtual bool Contains(const T& key)             const { return _data.Contains(key); }
                 /// <summary> Removes all of the data currently stored within this buffer. </summary>
                 virtual void Clear()                            override
                 {
                     if (IsEmpty()) { return; }
-                    Data.clear();
+                    _data.Clear();
                     NeedsUpdate(true);
                 }
+
                 virtual void Set(const T& key, const U& value)
                 {
-                    Data[key] = value;
+                    //_data[key] = value;
+                    _data.Insert(key, value);
                     NeedsUpdate(true);
                 }
 
                 virtual void Remove(const T& key)
                 {
-                    if (Data.count(key))
+                    if (Contains(key))
                     {
-                        Data.erase(key);
+                        //_data.erase(key);
+                        _data.Remove(key);
                         NeedsUpdate(true);
                     }
                 }
@@ -54,33 +64,31 @@ namespace Cyclone
                     if (!NeedsUpdate()) { return; }
                     GraphicsBuffer::Update();
 
-                    U* handles = (U*)GraphicsBuffer::Map();
-                    if (!handles)
-                    {
-                        Console::WriteLine("Failed to map and update the graphics buffer GPU-side storage.");
-                        return;
-                    }
-
                     uint idx = 0;
-                    for (const auto& kvp : Data)
-                        handles[idx++] = kvp.second;
+                    U* handles = (U*)GraphicsBuffer::Map(BufferAccessIntents::Write | BufferAccessIntents::Invalidate);
+                    for (const U& d : _data.Values())
+                        handles[idx++] = d;
 
                     GraphicsBuffer::Unmap();
                 }
 
-
+                
 
             protected:
+
+                //const std::map<T, U>& Data()        const { return _data; }
+                const BST<T, U>& Data() const { return _data; }
 
                 /** CONSTRUCTOR **/
                 MapBuffer(BufferTypes type) : GraphicsBuffer(type) { }
 
-
+                virtual const U& Get(const T& key) const { return _data[key]; }
 
             private:
 
                 /** DATA **/
-                std::map<T, U>  Data;
+                //std::map<T, U>  _data;
+                BST<T, U>   _data;
         };
     }
 }
