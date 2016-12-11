9fd3ebcea1785dc9cd8ea79f7b42f8c9193aac88
JoshGrooms <josh.grooms@gmail.com>
2016-12-11 09:33:46 -0800

    Updated for compatibility with the recent changes to the 'IMaterial' interface.
    Also added some property documentation.

--- OpenGL/Libraries/Material3D.h
+++ OpenGL/Libraries/Material3D.h
@@ -5,44 +5,49 @@
 #pragma once
 #include "GL/OpenGLAPI.h" 
 #include "Imaging/Color4.h"
-#include "Interfaces/IRenderable.h"
+#include "Interfaces/IMaterial.h"
 
 
 namespace Cyclone
 {
     namespace OpenGL
     {
-        class Material3D : public IMaterialEntity
+        class Material3D : public IMaterial
         {
+            
+            private:
+                
+                /** PROPERTY DATA **/
+                Color4              _primaryColor;
+                Color4              _secondaryColor;
+                const Texture3D*    _texture;
+
             public:
 
-                const Color4& PrimaryColor()    const override { return _primaryColor; }
-                const Color4& SecondaryColor()  const override { return _secondaryColor; }
-                const Texture3D* Texture()      const override { return _texture; }
+                /** PROPERTIES **/
+                /// <summary> Gets the primary color of the material. </summary>
+                const Color4& PrimaryColor()                        const override { return _primaryColor; }
+                /// <summary> Gets the secondary color of the material. </summary>
+                const Color4& SecondaryColor()                      const override { return _secondaryColor; }
+                /// <summary> Gets a pointer to the texture of a material. </summary>
+                const Texture3D* Texture()                          const override { return _texture; }
 
+                /// <summary> Sets the primary color of the material. </summary>
                 Material3D& PrimaryColor(const Color4& value)       { _primaryColor = value; return *this; }
+                /// <summary> Sets the secondary color of the material. </summary>
                 Material3D& SecondaryColor(const Color4& value)     { _secondaryColor = value; return *this; }
+                /// <summary> Sets the texture of a material. </summary>
                 Material3D& Texture(const Texture3D* value)         { _texture = value; return *this; }
 
 
 
+                /** CONSTRUCTOR **/
                 Material3D() :
                     _texture(nullptr)
                 {
 
                 }
 
-
-
-
-
-
-            private:
-                Color4              _primaryColor;
-                Color4              _secondaryColor;
-                const Texture3D*    _texture;
-
-            
         };
     }
 }
