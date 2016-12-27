d080c85dd3d45126986779d0b8f96f9fb2d8747e
JoshGrooms <josh.grooms@gmail.com>
2016-12-27 06:08:12 -0800

    Converted vector reference outputs to concrete instances.
    Returning references pretty much forced internal storage to use
    vectors, which may not always be ideal. This approach is less
    efficient, but allows a lot more flexibility.

--- OpenGL/Interfaces/IGeometric.h
+++ OpenGL/Interfaces/IGeometric.h
@@ -28,21 +28,21 @@ namespace Cyclone
 
                 /** PROPERTIES **/
                 /// <summary> Gets the bounding volume of the 3D geometric shape. </summary>
-                virtual const Volume& Bounds()                          const = 0;
+                virtual const Volume& Bounds()      const = 0;
                 /// <summary> Gets an array of indices that define the order in which <see cref="Points"/> is rendered. </summary>
                 /// <remarks>
                 ///     Not all geometry will be composed of indexable control points or vertices. In those cases, this method will
                 ///     return an empty array.
                 /// </remarks>
-                virtual const Vector<uint>& Indices()                   const = 0;
+                virtual Vector<uint> Indices()      const = 0;
                 /// <summary> Gets an array of values that map each geometric point onto some other external resource. </summary>
                 /// <remarks>
                 ///     Typically, these values will be used to map the points of some shape onto textures, although use of other
                 ///     resources such as buffers is possible as well.
                 /// </remarks>
-                virtual const Vector<Vector3>& Mapping()                const = 0;
+                virtual Vector<Vector3> Mapping()   const = 0;
                 /// <summary> Gets an array of normal vectors associated with each point of the geometric shape. </summary>
-                virtual const Vector<Vector3>& Normals()                const = 0;
+                virtual Vector<Vector3> Normals()   const = 0;
                 /// <summary> Gets an array of points that define a 3D geometric shape. </summary>
                 /// <remarks>
                 ///     This property is meant to be interpretted in one of a few possible ways. Most commonly, the name 'Points' will
@@ -50,9 +50,9 @@ namespace Cyclone
                 ///     for rendering. However, it may also refer to the control points of either a 3D patch object or 2D path object
                 ///     (note the lack of a 'c' in the latter; though similarly named, the two are very different things).
                 /// </remarks>
-                virtual const Vector<Vector3>& Points()                 const = 0;
+                virtual Vector<Vector3> Points()    const = 0;
                 /// <summary> Gets an enumerator that represents the type of primitive geometry defined by the <see cref="Points"/> property. </summary>
-                virtual VertexTopologies Topology()                     const = 0;
+                virtual VertexTopologies Topology() const = 0;
 
 
 
