86f971a6f168a41cdfa4cc6cdff918f7bf99747a
JoshGrooms <josh.grooms@gmail.com>
2016-12-31 07:44:42 -0800

    Cleaned up unnecessary dependencies.

--- OpenGL/Scenes/Scene3D.cpp
+++ OpenGL/Scenes/Scene3D.cpp
@@ -1,9 +1,4 @@
-#include "Collections/List.h"
-#include "Interfaces/IMaterial.h"
-#include "Interfaces/IRenderable.h"
-#include "Pipelines/RenderStage3D.h"
 #include "Scenes/Scene3D.h"
-#include "Spatial/Transform.h"
 
 
 
@@ -11,7 +6,6 @@ namespace Cyclone
 {
     namespace OpenGL
     {
-
 
         /** CONSTRUCTOR **/
         Scene3D::Scene3D(const string& name) :
