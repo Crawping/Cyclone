1299b45fb7a65ae063d465e1f995e1a0926b20a0
JoshGrooms <josh.grooms@gmail.com>
2017-01-11 17:02:55 -0800

    Continuing work on the new 'ListVector' class.

--- Testing/ListVector.cpp
+++ Testing/ListVector.cpp
@@ -49,4 +49,31 @@ TEST_F(_ListVector, ElementInsertion)
     ASSERT_EQ(_l1(129), 128);
     ASSERT_EQ(_l1.Count(), 130);
     ASSERT_EQ(_l1.Capacity(), 256);
+
+    _l1.Insert(-5, 5);
+    ASSERT_EQ(_l1(0), 5);
+    ASSERT_EQ(_l1(15), 11);
+    ASSERT_EQ(_l1(16), 10);
+    ASSERT_EQ(_l1(134), 128);
+    ASSERT_EQ(_l1.Count(), 135);
+    ASSERT_EQ(_l1.Capacity(), 256);
+
+    _l1.Insert(1, 6);
+    ASSERT_EQ(_l1(0), 5);
+    ASSERT_EQ(_l1(1), 6);
+    ASSERT_EQ(_l1(16), 11);
+    ASSERT_EQ(_l1(17), 10);
+    ASSERT_EQ(_l1(135), 128);
+    ASSERT_EQ(_l1.Count(), 136);
+    ASSERT_EQ(_l1.Capacity(), 256);
+
+    _l1.Insert(-128, 4);
+    ASSERT_EQ(_l1(0), 4);
+    ASSERT_EQ(_l1(128), 5);
+    ASSERT_EQ(_l1(129), 6);
+    ASSERT_EQ(_l1(144), 11);
+    ASSERT_EQ(_l1(145), 10);
+    ASSERT_EQ(_l1(263), 128);
+    ASSERT_EQ(_l1.Count(), 264);
+    ASSERT_EQ(_l1.Capacity(), 512);
 }
--- Utilities/Collections/ListVector.h
+++ Utilities/Collections/ListVector.h
@@ -33,98 +33,201 @@ namespace Cyclone
                 {
 
                 }
+                ListVector(const ICollection<T>& values) :
+                    _count(0),
+                    _index(n / 4),
+                    Data(nextpow2(values.Count()))
+                {
+
+                }
+                ListVector(std::initializer_list<T> values) :
+                    _count(0),
+                    _index(n / 4),
+                    Data(nextpow2(values.size()))
+                {
+                    uint idx = 0;
+                    for (const T& v : values)
+                        Set(idx++, v);
+                }
+
 
 
 
                 /** UTILITIES **/
-                virtual void Append(const T& value)                     { Insert(Count(), value); }
-                virtual void Append(const ICollection<T>& values)       { Insert(Count(), values); }
+                virtual void Append(const T& value)                             { Insert(Count(), value); }
+                virtual void Append(const ICollection<T>& values)               { Insert(Count(), values); }
                 virtual void Insert(int idx, const T& value)
                 {
-                    int idxVal = _index + idx;
+                    InsertBlock(idx, 1);
                     if (idx < 0)
-                    {
-                        if (idxVal < 0)
-                        {
-                            uint offset = _index - idx;
-                            uint newCount = offset + _count;
-                            idxVal = newCount / 4;
-                            Reallocate(2 * newCount, offset);
-                        }
-
-                        Data(idxVal) = value;
-                        //_count = 
-
-                    }
-
-                    if (idx >= _count)
-                    {
-                        if (idxVal >= Capacity())
-                        {
-                            uint newCap = 2 * Capacity();
-                            while (idxVal >= newCap) { newCap *= 2; }
-                            Reallocate(newCap, _index);
-                        }
-
-                        Data(idxVal) = value;
-                        _count = idx + 1;
-                    }
+                        Data(_index) = value;
                     else
-                    {
-                        if (_index + _count + 1 >= Capacity())
-                            Reallocate(2 * Capacity(), _index);;
+                        Data(_index + idx) = value;
+                    
 
-                        uint nToMove = _count - idx;
-                        Move(_index + idx, nToMove, 1);
-                        Data(idxVal) = value;
-                        _count++;
-                    }
+                    //if (idx < 0)
+                    //{
+                    //    if (idxData < 0)
+                    //    {
+                    //        uint newCap = 2 * Capacity();
+                    //        while ((_count - idx) >= newCap) { newCap *= 2; }
+                    //        Reallocate(newCap, -idx);
+                    //        idxData = _index;
+                    //    }
+
+                    //    _count = _count - idx;
+                    //    _index = idxData;
+                    //    Data(idxData) = value;
+                    //}
+                    //else if (idx >= _count)
+                    //{
+                    //    if (idxData >= Capacity())
+                    //    {
+                    //        uint newCap = 2 * Capacity();
+                    //        while (idxData >= newCap) { newCap *= 2; }
+                    //        Reallocate(newCap);
+                    //        idxData = _index + idx;
+                    //    }
+
+                    //    Data(idxData) = value;
+                    //    _count = idx + 1;
+                    //}
+                    //else
+                    //{
+                    //    if (_index + _count + 1 >= Capacity())
+                    //        Reallocate(2 * Capacity());
+
+                    //    uint nToMove = _count - idx;
+                    //    Move(_index + idx, nToMove, 1);
+                    //    Data(idxData) = value;
+                    //    _count++;
+                    //}
                 }
-                virtual void Insert(uint idx, const ICollection<T>& values)
+                virtual void Insert(int idx, const ICollection<T>& values)
                 {
 
                 }
-                virtual void Prepend(const T& value)
+                virtual void Prepend(const T& value)                            { Insert(0, value); }
+                virtual void Prepend(const ICollection<T>& values)              { Insert(0, values); }
+                virtual void Set(int idx, const T& value)
                 {
-
+                    int idxData = _index + idx;
+                    if ( (idxData < 0) || (idxData >= Capacity()) )
+                        return Insert(idx, value);
+                    Data(idxData) = value;
                 }
-                virtual void Prepend(const ICollection<T>& values)
-                {
 
-                }
-                
 
-                virtual const T& operator ()(uint index) const override
-                {
-                    return Data(_index + index);
-                }
+
+                /** OPERATORS **/
+                virtual T& operator ()(uint index)          { return Data(_index + index); }
+                virtual const T& operator ()(uint index)    const override { return Data(_index + index); }
 
 
 
             protected:
 
-                virtual void Move(uint idx, uint count, int n)
+                virtual void InsertBlock(int idx, uint n)
+                {
+                    int idxData = _index + idx;
+                    uint count = 0, offset = 0;
+
+                    if (idx < 0)
+                    {
+                        count = _index - idx + n + _count;
+                        offset = _index - idx + n;
+                    }
+                    else if (idx > _count)
+                    {
+                        count = _count + idx + n;
+                    }
+                    else
+                    {
+                        count = _count + n;
+                        Move(idxData, _count - idxData, n);
+                    }
+
+                    /*needsReallocation = (count >= Capacity());*/
+                    Reallocate(count, offset);
+
+
+                    /*if (idx < 0)
+                        if (idxData < 0)
+                        {
+                            count = _index - idx + n + _count;
+                            offset = -idx;
+                        }
+                        else if (idxData)
+                        {
+
+                        }
+                        else
+                        {
+                            _index += idx;
+                            _count -= idx;
+                            return;
+                        }
+                    else if (idx >= _count)
+                        if (idxData >= Capacity())
+                            count = idx + n;
+                         else
+                        {
+                            _count = idx + n;
+                            return;
+                        }
+                    else
+                    {
+                        _count += n;
+                        Move(idx, _count - idx, n);
+                        return;
+                    }*/
+
+                    //if (idxData < 0)
+                    //{
+                    //    newCount = _index - idx + _count;
+                    //    offset = -idx;
+                    //}
+                    //else if (idxData >= Capacity())
+                    //    newCount = idx - _index + n;
+                    //else
+                    //    return Move(idx, _count - idx, n);
+
+                    //if (newCount >= Capacity())
+                    //    Reallocate(nextpow2(newCount), offset);
+                    //else
+                    //    Move(_index, )
+
+                    //Reallocate(count, offset);
+                }
+
+                virtual void Move(int idx, uint count, int n)
                 {
                     //  [ 0, 0, 0, 0, a, b, c, d, 0, 0, 0, 0 ]
 
                     if (n == 0) { return; }
-
+                    uint idxData = _index + idx;
                     if (n > 0)
-                        for (uint a = idx + count + n - 1; a > idx; a--)
+                        for (uint a = idxData + count + n - 1; a > idxData; a--)
                             Data(a) = Data(a - n);
                     else
-                        for (uint a = idx; a < count; a++)
+                        for (uint a = idxData; a < count; a++)
                             Data(a + n) = Data(a);
+
+                    _count += n;
                 }
 
-                virtual void Reallocate(uint n, int offset)
+                virtual void Reallocate(uint n, int offset = 0)
                 {
-                    Vector<T> newData(n);
-                    uint idx = (n / 4) + offset;
+                    if (n <= Capacity()) { return; }
+                    uint newCap = nextpow2(n);
+
+                    Vector<T> newData(newCap);
+                    uint idx = (newCap / 4) + offset;
                     for (uint a = _index; a < _index + _count; a++)
                         newData(idx++) = Data(a);
 
-                    _index = idx;
+                    _count = n;
+                    _index = newCap / 4;
                     Data = newData;
                 }
 
@@ -135,9 +238,6 @@ namespace Cyclone
                 uint _index;
 
                 Vector<T> Data;
-
-
-
         };
     }
 }
