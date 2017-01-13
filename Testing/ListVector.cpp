74594ac97e8e2f318402ba13908d0d1454e94ac4
JoshGrooms <josh.grooms@gmail.com>
2017-01-13 08:58:42 -0800

    Finished implementing the prototype 'ListVector' data structure.

--- Testing/ListVector.cpp
+++ Testing/ListVector.cpp
@@ -12,68 +12,123 @@ using namespace Cyclone::Utilities;
 class _ListVector : public testing::Test
 {
     protected:
+        
+        Vector<int>         _v1 = { 0, 1, 2, 3, 4, 5 };
+        Vector<int>         _v2;
 
         ListVector<int>     _l0;
         ListVector<int>     _l1;
 
 
-        _ListVector()
+        _ListVector() : 
+            _v2(512)
         {
-
+            _v2.Fill(9);
         }
 
 };
 
 
 
+/** CONSTRUCTION TESTS **/
 TEST_F(_ListVector, DefaultConstruction)
 {
     ASSERT_EQ(_l0.Count(), 0);
     ASSERT_EQ(_l0.Capacity(), 128);
 }
 
+
+
+
+/** UTILITY TESTS **/
+TEST_F(_ListVector, CollectionAppending)
+{
+    _l1.Append(_v1);
+    ASSERT_EQ(_l1.Count(), _v1.Count());
+    ASSERT_EQ(_l1.Capacity(), 128);
+
+    for (uint a = 0; a < _v1.Count(); a++)
+        ASSERT_EQ(_l1(a), _v1(a));
+
+    _l1.Append(_v2);
+    ASSERT_EQ(_l1.Count(), _v1.Count() + _v2.Count());
+    ASSERT_EQ(_l1.Capacity(), nextpow2(_v1.Count() + _v2.Count()));
+
+    for (uint a = 0; a < _v1.Count(); a++)
+        ASSERT_EQ(_l1(a), _v1(a));
+    for (uint a = _v1.Count(); a < _l1.Count(); a++)
+        ASSERT_EQ(_l1(a), _v2(a - _v1.Count()));
+}
+TEST_F(_ListVector, CollectionInsertion)
+{
+    _l1.Append(_v2);
+    _l1.Insert(256, _v1);
+    ASSERT_EQ(_l1.Count(), _v1.Count() + _v2.Count());
+    ASSERT_EQ(_l1.Capacity(), nextpow2(_v1.Count() + _v2.Count()));
+
+    for (uint a = 0; a < 256; a++)
+        ASSERT_EQ(_l1(a), _v2(a));
+    for (uint a = 256; a < 256 + _v1.Count(); a++)
+        ASSERT_EQ(_l1(a), _v1(a - 256));
+    for (uint a = 256 + _v1.Count(); a < _l1.Count(); a++)
+        ASSERT_EQ(_l1(a), _v2(a - _v1.Count()));
+}
+TEST_F(_ListVector, CollectionPrepending)
+{
+    _l1.Prepend(_v1);
+    ASSERT_EQ(_l1.Count(), _v1.Count());
+    ASSERT_EQ(_l1.Capacity(), 128);
+
+    for (uint a = 0; a < _l1.Count(); a++)
+        ASSERT_EQ(_l1(a), _v1(a));
+
+    _l1.Prepend(_v2);
+    ASSERT_EQ(_l1.Count(), _v1.Count() + _v2.Count());
+    ASSERT_EQ(_l1.Capacity(), nextpow2(_v1.Count() + _v2.Count()));
+
+    for (uint a = 0; a < _v2.Count(); a++)
+        ASSERT_EQ(_l1(a), _v2(a));
+    for (uint a = _v2.Count(); a < _l1.Count(); a++)
+        ASSERT_EQ(_l1(a), _v1(a - _v2.Count()));
+}
 TEST_F(_ListVector, ElementInsertion)
 {
-    _l1.Insert(10, 10);
-    ASSERT_EQ(_l1(10), 10);
-    ASSERT_EQ(_l1.Count(), 11);
+    _l1.Insert(0, 0);
+    ASSERT_EQ(_l1(0), 0);
+    ASSERT_EQ(_l1.Count(), 1);
 
-    _l1.Insert(128, 128);
-    ASSERT_EQ(_l1(128), 128);
-    ASSERT_EQ(_l1.Count(), 129);
-    ASSERT_EQ(_l1.Capacity(), 256);
+    _l1.Insert(1, 20);
+    ASSERT_EQ(_l1(1), 20);
+    ASSERT_EQ(_l1.Count(), 2);
+    ASSERT_EQ(_l1.Capacity(), 128);
 
-    _l1.Insert(10, 11);
-    ASSERT_EQ(_l1(10), 11);
-    ASSERT_EQ(_l1(11), 10);
-    ASSERT_EQ(_l1(129), 128);
-    ASSERT_EQ(_l1.Count(), 130);
-    ASSERT_EQ(_l1.Capacity(), 256);
+    _l1.Insert(1, 10);
+    ASSERT_EQ(_l1(0), 0);
+    ASSERT_EQ(_l1(1), 10);
+    ASSERT_EQ(_l1(2), 20);
+    ASSERT_EQ(_l1.Count(), 3);
+    ASSERT_EQ(_l1.Capacity(), 128);
 
-    _l1.Insert(-5, 5);
-    ASSERT_EQ(_l1(0), 5);
-    ASSERT_EQ(_l1(15), 11);
-    ASSERT_EQ(_l1(16), 10);
-    ASSERT_EQ(_l1(134), 128);
-    ASSERT_EQ(_l1.Count(), 135);
-    ASSERT_EQ(_l1.Capacity(), 256);
+    _l1.Insert(0, 30);
+    ASSERT_EQ(_l1(0), 30);
+    ASSERT_EQ(_l1(1), 0);
+    ASSERT_EQ(_l1(2), 10);
+    ASSERT_EQ(_l1(3), 20);
+    ASSERT_EQ(_l1.Count(), 4);
+    ASSERT_EQ(_l1.Capacity(), 128);
+}
+TEST_F(_ListVector, ElementRemoval)
+{
+    _l1.Append(_v1);
+    _l1.Remove(0);
+    ASSERT_EQ(_l1.Count(), _v1.Count() - 1);
+    ASSERT_EQ(_l1.First(), _v1(1));
 
-    _l1.Insert(1, 6);
-    ASSERT_EQ(_l1(0), 5);
-    ASSERT_EQ(_l1(1), 6);
-    ASSERT_EQ(_l1(16), 11);
-    ASSERT_EQ(_l1(17), 10);
-    ASSERT_EQ(_l1(135), 128);
-    ASSERT_EQ(_l1.Count(), 136);
-    ASSERT_EQ(_l1.Capacity(), 256);
+    _l1.Remove(_l1.Count() - 1);
+    ASSERT_EQ(_l1.Count(), _v1.Count() - 2);
+    ASSERT_EQ(_l1.Last(), _v1(_v1.Count() - 2));
 
-    _l1.Insert(-128, 4);
-    ASSERT_EQ(_l1(0), 4);
-    ASSERT_EQ(_l1(128), 5);
-    ASSERT_EQ(_l1(129), 6);
-    ASSERT_EQ(_l1(144), 11);
-    ASSERT_EQ(_l1(145), 10);
-    ASSERT_EQ(_l1(263), 128);
-    ASSERT_EQ(_l1.Count(), 264);
-    ASSERT_EQ(_l1.Capacity(), 512);
+    _l1.Remove(2);
+    ASSERT_EQ(_l1.Count(), _v1.Count() - 3);
+    ASSERT_EQ(_l1(2), _v1(4));
 }
--- Utilities/Collections/ListVector.h
+++ Utilities/Collections/ListVector.h
@@ -18,10 +18,16 @@ namespace Cyclone
         {
 
             public:
+                struct Iterator;
+
 
                 /** PROPERTIES **/
-                virtual uint Capacity() const { return Data.Count(); }
-                virtual uint Count()    const { return _count; }
+                virtual uint Capacity()         const { return Data.Count(); }
+                virtual uint Count()            const { return _count; }
+                virtual T& First()              { return Data(_index); }
+                virtual const T& First()        const { return Data(_index); }
+                virtual T& Last()               { return Data(_index + _count - 1); }
+                virtual const T& Last()         const { return Data(_index + _count - 1); }
 
 
 
@@ -52,60 +58,111 @@ namespace Cyclone
 
 
 
-
                 /** UTILITIES **/
                 virtual void Append(const T& value)                             { Insert(Count(), value); }
                 virtual void Append(const ICollection<T>& values)               { Insert(Count(), values); }
-                virtual void Insert(int idx, const T& value)
+                virtual void Clear()
+                {
+                    _count = 0;
+                    _index = Capacity() / 2;
+                }
+                virtual void Insert(uint idx, const T& value)
                 {
                     InsertBlock(idx, 1);
-                    if (idx < 0)
-                        Data(_index) = value;
-                    else
-                        Data(_index + idx) = value;
+                    Data(_index + idx) = value;
                 }
-                virtual void Insert(int idx, const ICollection<T>& values)
+                virtual void Insert(uint idx, const ICollection<T>& values)
                 {
-
+                    InsertBlock(idx, values.Count());
+                    for (uint a = 0; a < values.Count(); a++)
+                        Data(_index + idx++) = values(a);
                 }
                 virtual void Prepend(const T& value)                            { Insert(0, value); }
                 virtual void Prepend(const ICollection<T>& values)              { Insert(0, values); }
-                virtual void Set(int idx, const T& value)
+                virtual void Remove(uint idx, uint n = 1)
+                {
+                    RemoveBlock(idx, n);
+                }
+                virtual void Set(uint idx, const T& value)
                 {
                     int idxData = _index + idx;
                     if ( (idxData < 0) || (idxData >= Capacity()) )
                         return Insert(idx, value);
                     Data(idxData) = value;
                 }
-
+                virtual void Swap(uint idxFirst, uint idxSecond)
+                {
+                    Data.Swap(_index + idxFirst, _index + idxSecond);
+                }
 
 
                 /** OPERATORS **/
+                virtual Iterator begin()                    { return Iterator(_index, &Data); }
+                virtual Iterator end()                      { return Iterator(Count(), &Data); }
                 virtual T& operator ()(uint index)          { return Data(_index + index); }
                 virtual const T& operator ()(uint index)    const override { return Data(_index + index); }
 
-
-
             protected:
 
+                /** PROPERTIES **/
+                uint LeftMargin()       const { return _index; }
+                uint RightMargin()      const { return Capacity() - _index - Count(); }
+
+
+
+                /** UTILITIES **/
                 virtual void InsertBlock(uint idx, uint n)
                 {
                     if (n == 0) { return; }
-                    uint count = _count, offset = 0;
 
+                    idx = clamp(idx, (uint)0, _count);
+                    uint newCount = Count() + n;
+                    Reallocate(Count() + n);
 
-
-                    if (idx < _count)
-                        Move(idx, _count - idx, n);
-
-                    Reallocate(count + n, offset);
+                    if (idx == 0)
+                    {
+                        uint nmoved = Math::Min(n, LeftMargin());
+                        Move(idx, Count(), n - nmoved);
+                        _index -= nmoved;
+                        _count += nmoved;
+                    }
+                    else if (idx <= Count() / 2)
+                    {
+                        uint nmoved = Math::Min(n, LeftMargin());
+                        Move(0, idx, -(int)nmoved);
+                        Move(idx, Count() - idx, n - nmoved);
+                    }
+                    else
+                    {
+                        uint nmoved = Math::Min(n, RightMargin());
+                        Move(idx, Count() - idx, nmoved);
+                        Move(0, idx, -(int)(n - nmoved));
+                    }
                 }
-
-                virtual void Redistribute(uint idx, uint offset)
+                virtual void RemoveBlock(uint idx, uint n)
                 {
+                    if (n == 0) { return; }
 
+                    idx = clamp(idx, (uint)0, Count());
+                    if (idx == 0)
+                    {
+                        _index += n;
+                        _count -= n;
+                    }
+                    else if (idx == Count() - n)
+                        _count -= n;
+                    else if (idx <= Count() / 2)
+                    {
+                        Move(0, idx, n);
+                        _index += n;
+                        _count -= 2 * n;
+                    }
+                    else
+                    {
+                        Move(idx, Count() - idx, -(int)n);
+                        _count -= 2 * n;
+                    }
                 }
-
 
             private:
 
@@ -116,37 +173,72 @@ namespace Cyclone
 
 
 
-                virtual void Move(int idx, int offset)
+                /** UTILITIES **/
+                void Move(uint idx, uint count, int offset)
                 {
-                    if (offset == 0) { return; }
+                    if (!(offset && count && Count())) { _count += abs(offset); return; }
 
-                    uint idxData = _index + idx;
-                    if (n > 0)
-                        for (uint a = idxData + count + n - 1; a > idxData; a--)
-                            Data(a) = Data(a - n);
+                    uint idxStart = _index + idx;
+                    uint idxEnd = idxStart + count - 1;
+
+                    if (offset > 0)
+                    {
+                        for (uint a = idxEnd; a > idxStart; a--)
+                            Data(a + offset) = Data(a);
+                        Data(idxStart + offset) = Data(idxStart);
+                    }
                     else
-                        for (uint a = idxData; a < count; a++)
-                            Data(a + n) = Data(a);
+                    {
+                        for (uint a = idxStart; a <= idxEnd; a++)
+                            Data(a + offset) = Data(a);
+                        _index += offset;
+                    }
 
-                    _count += n;
+                    _count += abs(offset);
                 }
-                void Reallocate(uint n, uint idx = 0, uint offset = 0)
+                void Reallocate(uint n)
                 {
-                    if (n <= Capacity())
-                        return Move(idx, offset);
+                    if (n <= Capacity()) { return; }
 
-                    //if (n <= Capacity()) { _count = n; _index += offset; return; }
                     uint newCap = nextpow2(n);
-
                     Vector<T> newData(newCap);
-                    uint idx = (newCap / 4) + offset;
+                    uint idx = (newCap / 4);
+
                     for (uint a = _index; a < _index + _count; a++)
                         newData(idx++) = Data(a);
 
-                    _count = n;
                     _index = newCap / 4;
                     Data = newData;
                 }
+
+
+                struct Iterator //: public ICollectionIterator<T>
+                {
+                    public:
+
+                        uint Index() const { return _index; }
+
+                        Iterator(uint idx, Vector<T>* collection) : 
+                            _index(idx),
+                            Collection(collection)
+                        {
+
+                        }
+
+                        bool operator ==(const Iterator& other) const
+                        {
+                            return (Index() == other.Index()) && (Collection == other.Collection);
+                        }
+                        bool operator !=(const Iterator& other) const { return !operator ==(other); }
+                        T& operator *()                 const { return (*Collection)(Index()); }
+                        Iterator& operator ++()         { _index++; return *this; }
+
+                    private:
+
+                        uint _index;
+                        Vector<T>* Collection;
+
+                };
 
         };
     }
