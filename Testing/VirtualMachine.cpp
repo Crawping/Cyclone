7dc223b9eb8c59a589ecc8e13245888b1f298f3c
JoshGrooms <josh.grooms@gmail.com>
2017-01-24 09:27:02 -0800

    Implemented a bunch of new execution tests for virtual machines.
    For right now, these are only testing the basic functionality of
    virtual machines (and, by necessity, virtual tables). More advanced
    tests will be written soon.

--- Testing/VirtualMachine.cpp
+++ Testing/VirtualMachine.cpp
@@ -4,6 +4,7 @@
 
 
 #include "VirtualMachine.h"
+#include "Utilities.h"
 #include <gtest/gtest.h>
 
 using namespace Cyclone::VM;
@@ -15,43 +16,171 @@ class _VirtualMachine : public testing::Test
 {
     protected:
 
-        VirtualMachine  VM;
-        VirtualTable    VT;
+        Vector<uint, 5>     _i1 = 0;
+        Vector<string, 5>   _s1 = { "Variable0", "Variable1", "Variable2", "Variable3", "Variable4" };
+        Vector<int, 5>      _v1 = { 0, 10, 20, 30, 40, 50 };
 
-        _VirtualMachine() { VM.Data(&VT); }
+        VirtualClass        _c1;
+        VirtualProperty     _p1;
+
+        VirtualMachine      _m0;
+        VirtualMachine      _m1;
+        VirtualTable        _t0;
+        VirtualTable        _t1;
+
+
+
+        _VirtualMachine()
+        {
+            _m0.Data(&_t0);
+            _m1.Data(&_t1);
+
+            for (uint a = 0; a < _v1.Count(); a++)
+            {
+                _i1(a) = _t1.Insert(_s1(a));
+                _t1.Set(_i1(a), _v1(a));
+            }
+
+            _c1 = VirtualClass(_i1(1));
+            _p1 = VirtualProperty(_i1(2));
+            _c1.Insert(_p1);
+
+            _t1.Insert(_c1);
+            _t1.Set(_c1.ID(), _p1.ID(), _i1(3), _v1(3));
+        }
 
 };
 
 
 
+/** CONSTRUCTION TESTS **/
 TEST_F(_VirtualMachine, DefaultConstruction)
 {
-    ASSERT_EQ(VM.Data(), &VT);
+    ASSERT_EQ(_m0.Data(), &_t0);
 }
 
-TEST_F(_VirtualMachine, Execution)
+
+
+/** EXECUTION TESTS **/
+TEST_F(_VirtualMachine, Addition)
 {
-    Vector<string, 3> varNames = { "variable1", "variable2", "variable3" };
-    Vector<uint, 3> varIDs = 0;
-    for (uint a = 0; a < 3; a++)
-        varIDs(a) = VT.Insert(varNames(a));
-
-    VT.Set(varIDs(0), 10);
-    VT.Set(varIDs(1), 20);
-    VT.Set(varIDs(2), 00);
-
-    ASSERT_EQ(VT.Get(varIDs(0)), 10);
-    ASSERT_EQ(VT.Get(varIDs(1)), 20);
-
     Vector<Instruction> cmds =
     {
-        { Instructions::Load, varIDs(0) },
-        { Instructions::Load, varIDs(1) },
-        { Instructions::Add, { } },
-        { Instructions::Store, varIDs(2) },
+        { Instructions::Load,       _i1(1) },
+        { Instructions::Load,       _i1(2) },
+        { Instructions::Add,             0 },
+        { Instructions::Store,      _i1(3) },
     };
 
-    VM.Execute(cmds);
+    _m1.Execute(cmds);
+    ASSERT_EQ(_t1.Get(_i1(3)), _v1(1) + _v1(2));
+}
+TEST_F(_VirtualMachine, Comparison)
+{
+    Vector<Instruction> cmds =
+    {
+        { Instructions::Load,       _i1(1) },
+        { Instructions::Load,       _i1(2) },
+        { Instructions::Compare,         0 },
+        { Instructions::Store,      _i1(3) },
+    };
 
-    ASSERT_EQ(VT.Get(varIDs(2)).Value, 30);
+    _m1.Execute(cmds);
+    ASSERT_EQ(_t1.Get(_i1(3)), compare(_v1(1), _v1(2)));
+}
+TEST_F(_VirtualMachine, Decrementing)
+{
+    Vector<Instruction> cmds =
+    {
+        { Instructions::Load,       _i1(1) },
+        { Instructions::Decrement,       0 },
+        { Instructions::Decrement,       0 },
+        { Instructions::Store,      _i1(3) },
+    };
+
+    _m1.Execute(cmds);
+    ASSERT_EQ(_t1.Get(_i1(3)), _v1(1) - 2);
+}
+TEST_F(_VirtualMachine, Deletion)
+{
+    Vector<Instruction> cmds =
+    {
+        { Instructions::Delete,     _i1(1) },
+        { Instructions::Delete,     _i1(2) },
+        { Instructions::Load,       _i1(1) },
+        { Instructions::Load,       _i1(2) },
+        { Instructions::Compare,         0 },
+        { Instructions::Store,      _i1(3) },
+    };
+
+    _m1.Execute(cmds);
+    ASSERT_EQ(_t1.Get(_i1(1)), Literal());
+    ASSERT_EQ(_t1.Get(_i1(2)), Literal());
+    ASSERT_EQ(_t1.Get(_i1(3)), 0);
+}
+TEST_F(_VirtualMachine, Division)
+{
+    Vector<Instruction> cmds =
+    {
+        { Instructions::Load,       _i1(1) },
+        { Instructions::Load,       _i1(2) },
+        { Instructions::Divide,          0 },
+        { Instructions::Store,      _i1(3) },
+    };
+
+    _m1.Execute(cmds);
+    ASSERT_EQ((int)_t1.Get(_i1(3)).Value, _v1(1) / _v1(2));
+}
+TEST_F(_VirtualMachine, Incrementing)
+{
+    Vector<Instruction> cmds =
+    {
+        { Instructions::Load,       _i1(1) },
+        { Instructions::Increment,       0 },
+        { Instructions::Increment,       0 },
+        { Instructions::Store,      _i1(3) },
+    };
+
+    _m1.Execute(cmds);
+    ASSERT_EQ(_t1.Get(_i1(3)), _v1(1) + 2);
+}
+TEST_F(_VirtualMachine, Multiplication)
+{
+    Vector<Instruction> cmds =
+    {
+        { Instructions::Load,       _i1(1) },
+        { Instructions::Load,       _i1(2) },
+        { Instructions::Multiply,        0 },
+        { Instructions::Store,      _i1(3) },
+    };
+
+    _m1.Execute(cmds);
+    ASSERT_EQ(_t1.Get(_i1(3)), _v1(1) * _v1(2));
+}
+TEST_F(_VirtualMachine, ObjectInteraction)
+{
+    ASSERT_EQ(_t1.Get(_c1.ID(), _p1.ID(), _i1(3)), _v1(3));
+    Vector<Instruction> cmds =
+    {
+        { Instructions::Load,       _i1(1) },
+        { Instructions::Get,        { _c1.ID(), _p1.ID(), _i1(3) } },
+        { Instructions::Multiply,        0 },
+        { Instructions::Set,        { _c1.ID(), _p1.ID(), _i1(3) } },
+    };
+
+    _m1.Execute(cmds);
+    ASSERT_EQ(_t1.Get(_c1.ID(), _p1.ID(), _i1(3)), _v1(1) * _v1(3));
+}
+TEST_F(_VirtualMachine, Subtraction)
+{
+    Vector<Instruction> cmds =
+    {
+        { Instructions::Load,       _i1(1) },
+        { Instructions::Load,       _i1(2) },
+        { Instructions::Subtract,        0 },
+        { Instructions::Store,      _i1(3) },
+    };
+
+    _m1.Execute(cmds);
+    ASSERT_EQ(_t1.Get(_i1(3)), _v1(1) - _v1(2));
 }
