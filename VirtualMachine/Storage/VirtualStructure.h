d16e6c3c41bc0d0f6bffa499040230095ebdb3d5
JoshGrooms <josh.grooms@gmail.com>
2017-02-08 06:07:53 -0800

    Began working on a new 'structure' type for the virtual machine.
    These will eventually act like structures in MATLAB or objects in
    JavaScript.

---
+++ VirtualMachine/Storage/VirtualStructure.h
@@ -0,0 +1,23 @@
+/* CHANGELOG
+ * Written by Josh Grooms on 20170201
+ */
+
+#pragma once
+
+
+
+namespace Cyclone
+{
+    namespace VM
+    {
+        class VirtualStructure
+        {
+            public:
+
+
+            private:
+
+
+        };
+    }
+}
