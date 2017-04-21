20d6f9388aa082c4854273b4985966894b033823
JoshGrooms <josh.grooms@gmail.com>
2017-04-21 07:56:44 -0700

    Drafted some documentation for the 'metaclass' class.

--- Utilities/Reflection/Type.h
+++ Utilities/Reflection/Type.h
@@ -19,21 +19,28 @@ namespace Cyclone
         using namespace Utilities;
 
 
-
+        /// <summary> A class that holds runtime-accessible information about other classes. </summary>
         class Metaclass
         {
             public:
                 
                 /** PROPERTIES **/    
+                /// <summary> Gets the unique numeric identifier associated with the class. </summary>
                 uint ID()               const { return _id; }
+                /// <summary> Gets whether the class is of a reference type. </summary>
                 bool IsReference()      const { return _isReference; }
+                /// <summary> Gets whether the class if of a pointer type. </summary>
                 bool IsPointer()        const { return _isPointer; }
+                /// <summary> Gets the human-readable string name of the class. </summary>
                 const string& Name()    const { return _name; }
+                /// <summary> Gets the number of bytes required to store an instance of the class. </summary>
                 uint Size()             const { return _size; }
 
 
 
                 /** STATIC CONSTRUCTOR **/
+                /// <summary> Constructs a new metaclass object containing information about a specific type. </summary>
+                /// <typeparam name="T"> The type for which the metaclass is to be constructed. </typeparam>
                 template<typename T> static Metaclass Create()
                 {
                     const auto& type = typeid(Meta::Dereference<T>::Class);
@@ -53,22 +60,28 @@ namespace Cyclone
 
 
                 /** UTILITIES **/
+                /// <summary> Determines whether the class contains a specific field. </summary>
+                /// <returns> A Boolean <c>true</c> if the field is part of the class description, or <c>false</c> otherwise. </returns>
+                /// <param name="field"> A field object to be tested for inclusion in the class description. </param>
                 UtilitiesAPI bool Contains(const Field& field) const;
+                /// <summary> Adds a new field to the class description. </summary>
+                /// <param name="type"> Another metaclass object that describes the type of the field being added. </param>
+                /// <param name="name"> The human-readable string name of the field being added. </param>
                 UtilitiesAPI void Insert(const Metaclass& type, const string& name);
 
-                UtilitiesAPI static const Metaclass& Get(const string& name);
-                UtilitiesAPI static bool IsClass(const string& name);
 
+
+                /** STATIC UTILITIES **/
+                /// <summary> Gets the metaclass object that corresponds with the string name of the underlying type. </summary>
+                UtilitiesAPI static const Metaclass& Get(const string& name);
+                /// <summary> Determines whether a specific class has been registered with the reflection system. </summary>
+                UtilitiesAPI static bool IsClass(const string& name);
+                /// <summary> Determines whether a specific class has been registered with the reflection system. </summary>
                 template<typename T> static bool IsClass()
                 {
                     const auto& type = typeid(Meta::Dereference<T>::Class);
                     return IsClass(type.name());
                 }
-
-
-
-                /** OPERATORS **/
-
 
             private:
                 
