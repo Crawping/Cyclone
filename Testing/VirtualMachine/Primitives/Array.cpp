5d631120dcbac63262cff5760a12b14d8ed0ca5d
Josh Grooms <josh.grooms@gmail.com>
2017-05-03 06:28:05 -0700

    Began writing some unit tests for the 'Array' primitive.

---
+++ Testing/VirtualMachine/Primitives/Array.cpp
@@ -0,0 +1,29 @@
+/* CHANGELOG
+ * Written by Josh Grooms on 20170502
+ */
+
+#include "Primitives/Array.h"
+#include <gtest/gtest.h>
+
+using namespace Cyclone::VM::Virtual;
+
+
+
+class _Array : public testing::Test 
+{
+	protected:
+		
+		_Array()
+		{
+
+		}
+
+};
+
+
+
+/** CONSTRUCTION **/
+TEST_F(_Array, Construction)
+{
+
+}
