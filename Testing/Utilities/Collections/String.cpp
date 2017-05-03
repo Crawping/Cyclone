45f5d70bfbab340dc5abe85a84a47e79ef87b779
Josh Grooms <josh.grooms@gmail.com>
2017-05-03 06:27:25 -0700

    Wrote some basic unit tests for the new 'String' collection.

---
+++ Testing/Utilities/Collections/String.cpp
@@ -0,0 +1,48 @@
+/* CHANGELOG
+ * Written by Josh Grooms on 20170502
+ */
+
+#include "Collections/String.h"
+#include <gtest/gtest.h>
+
+using namespace Cyclone::Utilities;
+
+
+
+class _String : public testing::Test
+{
+	protected:
+		
+		std::string		_ss1;
+
+		String			_s0;
+		String			_s1;
+		String			_s2;
+
+
+		_String():
+			_ss1("Testing"),
+			_s1("Testing")
+		{
+			_s2 = _s1;
+		}
+};
+
+
+
+/** CONSTRUCTION **/
+TEST_F(_String, Construction)
+{
+	ASSERT_EQ(_s0.Count(),		0);
+	ASSERT_EQ(_s1.Count(),		_ss1.size());
+}
+
+
+
+
+/** UTILITIES **/
+TEST_F(_String, Indexing)
+{
+	for (uint a = 0; a < _ss1.size(); a++)
+		ASSERT_EQ(_s1(a),		_ss1[a]);
+}
