53fdb1a950be58561466da070fd14c72f2882727
JoshGrooms <josh.grooms@gmail.com>
2017-05-27 10:48:28 -0700

    Updated and documented the new 'Array' class.
    Wrote a new 'SubscriptsOf' method, implemented some compile-time
    assertions for safety, removed the 'Meta' dependency, wrote several new
    tests, and made a few other general formatting changes.

--- Testing/Utilities/Collections/Array.cpp
+++ Testing/Utilities/Collections/Array.cpp
@@ -55,19 +55,26 @@ TEST_F(_Array, Construction)
     ASSERT_EQ(_a32.Count(), 27);
     ASSERT_EQ(_a32.Rank(),  3);
 
-    ASSERT_EQ(_a40.Count(), Meta::Product(10, 20, 30, 40));
+    ASSERT_EQ(_a40.Count(), Math::Product(10, 20, 30, 40));
     ASSERT_EQ(_a40.Rank(),  4);
-
-    auto size = _a61.Size();
-    ASSERT_EQ(size.Count(), 6);
-    for (uint a = 0; a < size.Count(); a++)
-        ASSERT_EQ(size(a), a + 5);
 }
-TEST_F(_Array, Flattening)
+TEST_F(_Array, Counts)
 {
-    Array<double, 27> a32 = _a32.Flatten();
-    for (uint a = 0; a < _a32.Count(); a++)
-        ASSERT_EQ(a32(a), _a32(a));
+    using A61 = Array<float, 5, 6, 7, 8, 9, 10>;
+    constexpr uint c1 = A61::Count(0);
+    constexpr uint c2 = A61::Count(1);
+    constexpr uint c3 = A61::Count(2);
+    constexpr uint c4 = A61::Count(3);
+    constexpr uint c5 = A61::Count(4);
+    constexpr uint c6 = A61::Count(5);
+
+    ASSERT_EQ(c1, 5);
+    ASSERT_EQ(c1, Math::Product(5));
+    ASSERT_EQ(c2, Math::Product(5, 6));
+    ASSERT_EQ(c3, Math::Product(5, 6, 7));
+    ASSERT_EQ(c4, Math::Product(5, 6, 7, 8));
+    ASSERT_EQ(c5, Math::Product(5, 6, 7, 8, 9));
+    ASSERT_EQ(c6, Math::Product(5, 6, 7, 8, 9, 10));
 }
 TEST_F(_Array, Indexing)
 {
@@ -112,4 +119,60 @@ TEST_F(_Array, IndexByArray)
 
     for (uint a = 0; a < a2.Count(); a++)
         ASSERT_EQ(a2(a), ids(a) + 1);
+}
+TEST_F(_Array, IndexOf)
+{
+    ASSERT_EQ(_a31.IndexOf(0, 1, 2), 5);
+
+    uint idx = 0;
+    for (uint f = 0; f < _a61.Size(5); f++)
+        for (uint e = 0; e < _a61.Size(4); e++)
+            for (uint d = 0; d < _a61.Size(3); d++)
+                for (uint c = 0; c < _a61.Size(2); c++)
+                    for (uint b = 0; b < _a61.Size(1); b++)
+                        for (uint a = 0; a < _a61.Size(0); a++)
+                            ASSERT_EQ(_a61.IndexOf(a, b, c, d, e, f), idx++);
+
+    ASSERT_EQ(_a31.IndexOf(0, 1, 2, 0, 0, 0), 5);
+}
+TEST_F(_Array, Reshaping)
+{
+    constexpr Array<uint, 3, 2, 1> a1   = _u61;
+    constexpr Array<uint, 1, 2, 3> a2   = a1.Reshape<1, 2, 3>();
+    constexpr Array<uint, 6> a3         = a1.Flatten();
+
+    for (uint a = 0; a < a1.Count(); a++)
+    {
+        ASSERT_EQ(a2(a), a1(a));
+        ASSERT_EQ(a3(a), a1(a));
+    }
+
+    Array<double, 27> a32 = _a32.Flatten();
+    for (uint a = 0; a < _a32.Count(); a++)
+        ASSERT_EQ(a32(a), _a32(a));
+}
+TEST_F(_Array, Sizes)
+{
+    for (uint a = 0; a < _a31.Rank(); a++)
+        ASSERT_EQ(_a31.Size(a), a + 1);
+
+    auto size = _a61.Size();
+    ASSERT_EQ(size.Count(), 6);
+    for (uint a = 0; a < size.Count(); a++)
+        ASSERT_EQ(size(a), a + 5);
+}
+TEST_F(_Array, SubscriptsOf)
+{
+    constexpr Array<uint, 3> a2 = Array<uint, 1, 2, 3>::SubscriptsOf(5);
+    for (uint a = 0; a < 3; a++)
+        ASSERT_EQ(a2(a), a);
+
+    uint idx = 0;
+    for (uint f = 0; f < _a61.Size(5); f++)
+        for (uint e = 0; e < _a61.Size(4); e++)
+            for (uint d = 0; d < _a61.Size(3); d++)
+                for (uint c = 0; c < _a61.Size(2); c++)
+                    for (uint b = 0; b < _a61.Size(1); b++)
+                        for (uint a = 0; a < _a61.Size(0); a++)
+                            ASSERT_EQ(_a61.SubscriptsOf(idx++), (Array<uint, 6>{ a, b, c, d, e, f }));
 }
--- Utilities/Collections/Array.h
+++ Utilities/Collections/Array.h
@@ -4,7 +4,7 @@
 
 #pragma once
 #include "Collections/Vector.h"
-#include "Meta/Array.h"
+#include "Math/Math.h"
 
 
 
@@ -21,7 +21,7 @@ namespace Cyclone
 
                 /** STATIC DATA **/
                 constexpr static uint _rank                     = sizeof...(U);
-                constexpr static uint _count                    = Meta::Product(U...);
+                constexpr static uint _count                    = Math::Product(U...);
                 constexpr static uint _size[_rank]              = { U... };
 
 
@@ -42,7 +42,9 @@ namespace Cyclone
 
 
                 /** PROPERTIES **/
+                /// <summary> Gets the first element present in the array. </summary>
                 constexpr const T& First()                      const { return _values[0]; }
+                /// <summary> Gets the last element present inthe array. </summary>
                 constexpr const T& Last()                       const { return _values[Count() - 1]; }
 
 
@@ -54,30 +56,36 @@ namespace Cyclone
                 {
 
                 }
+                /// <summary> Constructs a new multidimensional array whose elements are initialized to a uniform value. </summary>
+                /// <param name="value"> The value to which all array elements will be initialized. </param>
                 constexpr Array(const T& value):
                     _values{ }
                 {
                     for (uint a = 0; a < _count; a++)
                         _values[a] = value;
                 }
+                /// <summary> Constructs a new multidimensional array by copying a native array of values. </summary>
+                /// <param name="values"> A native array of values to be copied into the newly constructed one. </param>
                 constexpr Array(const T values[_count]):
                     _values{ }
                 {
                     for (uint a = 0; a < _count; a++)
                         _values[a] = values[a];
                 }
-                constexpr Array(const std::initializer_list<T>& values):
+                /// <summary> Constructs a new multidimensional array whose elements are initialized to specific values. </summary>
+                /// <param name="values"> An initial list of values to copied into the new array. </param>
+                constexpr Array(const InitialList<T>& values):
                     _values{ }
                 {
                     for (uint a = 0; a < values.size(); a++)
                         _values[a] = *(values.begin() + a);
                 }
-
-                template<uint N, uint ... V>
-                constexpr Array(const Array<T, V...>& values, const Array<T, N>& indices):
+                /// <summary> Constructs a new multidimensional array by copying a specific set of elements from another array. </summary>
+                template<uint ... V>
+                constexpr Array(const Array<T, V...>& values, const Array<T, _count>& indices):
                     _values{ }
                 {
-                    for (uint a = 0; a < indices.Count(); a++)
+                    for (uint a = 0; a < _count; a++)
                         _values[a] = values(indices(a));
                 }
 
@@ -98,35 +106,54 @@ namespace Cyclone
                 /// <returns> A flattened array of values copied from the multidimensional array. </returns>
                 constexpr Array<T, _count> Flatten()                            const { return { _values }; }
                 /// <summary> Gets the size of the array across a specific dimension. </summary>
-                /// <param name="dimension"> A single array dimension. </param>
                 /// <returns> The size of the array along the inputted dimension. </returns>
-                constexpr static uint Size(uint dimension)
-                {
-                    return (dimension >= _rank) ? 1 : _size[dimension];
-                }
-                constexpr static Array<uint, _rank> SubscriptsOf(uint index)
-                {
-
-                }
-
+                /// <param name="dimension"> A single array dimension. </param>
+                constexpr static uint Size(uint dimension)                      { return (dimension >= _rank) ? 1 : _size[dimension]; }
+                /// <summary> Calculates a linear index that is equivalent to a set of array subscripts. </summary>
+                /// <returns> A linear array index that references the same location as the inputted subscripts. </returns>
+                /// <param name="subscripts"> Any number of unsigned integers representing the array subscripts to convert. </param>
+                /// <remarks> Subscripts into dimensions higher than the rank of the array are ignored. </remarks>
                 template<typename ... V>
-                constexpr static uint IndexOf(V ... indices)
+                constexpr static uint IndexOf(V ... subscripts)
                 { 
-                    return IndexOf( Array<uint, sizeof...(V)>{ uint(indices)... } );
+                    return IndexOf( Array<uint, sizeof...(V)>{ uint(subscripts)... } );
                 }
+                /// <summary> Calculates a linear index that is equivalent to a set of array subscripts. </summary>
+                /// <returns> A linear array index that references the same location as the inputted subscripts. </returns>
+                /// <param name="subscripts"> Any number of unsigned integers representing the array subscripts to convert. </param>
+                /// <remarks> Subscripts into dimensions higher than the rank of the array are ignored. </remarks>
                 template<uint N>
-                constexpr static uint IndexOf(const Array<uint, N>& indices)
+                constexpr static uint IndexOf(const Array<uint, N>& subscripts)
                 {
-                    uint idx = indices(0U);
-                    for (uint a = 1; a < indices.Count(); a++)
-                        idx += indices(a) * Count(a - 1);
+                    constexpr uint rank = Math::Min(N, _rank);
+                    uint idx = subscripts(0U);
+                    for (uint a = 1; a < rank; a++)
+                        idx += subscripts(a) * Count(a - 1);
                     return idx;
                 }
-
+                /// <summary> Copies the array into a new one with different dimensions. </summary>
                 template<uint ... V>
                 constexpr Array<T, V...> Reshape()                              const
                 {
-                    StaticAssert< Meta::Product
+                    static_assert(Math::Product(V...) == _count,
+                        "The number of elements in an array must not change during reshaping operations.");
+                    return { _values };
+                }
+                /// <summary> Calculates a set of subscripts that is equivalent to a given linear array index. </summary>
+                /// <returns> Array subscripts that reference the same location as the inputted linear index. </returns>
+                /// <param name="index"> A single linear index into the array. </param>
+                constexpr static Array<uint, _rank> SubscriptsOf(uint index)
+                {
+                    uint subs[_rank] = { 0 };
+                    uint count = 0;
+                    for (uint a = _rank - 1; a > 0; a--)
+                    {
+                        count = Count(a - 1);
+                        subs[a] = index / count;
+                        index %= count;
+                    }
+                    subs[0U] = index;
+                    return { subs };
                 }
 
 
@@ -134,13 +161,22 @@ namespace Cyclone
                 /** OPERATORS **/
                 constexpr const T* begin()                                      const { return &_values[0]; }
                 constexpr const T* end()                                        const { return begin() + Count(); }
-                virtual T& operator ()(uint index)                              { return _values[index]; }
+                constexpr T& operator ()(uint index)                            { return _values[index]; }
                 constexpr const T& operator ()(uint index)                      const { return _values[index]; }
 
                 template<typename ... V>
                 constexpr const T& operator ()(V ... indices)                   const { return _values[IndexOf(indices...)]; }
                 template<uint N>
                 constexpr Array<T, N> operator ()(const Array<T, N>& indices)   const { return Array<T, N>(*this, indices); }
+
+
+                constexpr bool operator ==(const Array<T, U...>& other)         const
+                {
+                    for (uint a = 0; a < _count; a++)
+                        if (_values[a] != other(a)) return false;
+                    return true;
+                }
+                constexpr bool operator !=(const Array<T, U...>& other)         const { return !operator ==(other); }
                 
         };
 
