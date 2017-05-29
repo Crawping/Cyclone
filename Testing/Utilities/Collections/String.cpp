82a5fb1487517965026ab365b67e9f36816d3fef
JoshGrooms <josh.grooms@gmail.com>
2017-05-29 11:18:51 -0700

    Added a test to ensure constant expressions are working in the 'String' class.

--- Testing/Utilities/Collections/String.cpp
+++ Testing/Utilities/Collections/String.cpp
@@ -33,6 +33,11 @@ class _String : public testing::Test
 /** CONSTRUCTION **/
 TEST_F(_String, Construction)
 {
+    constexpr String s1 = "Testing";
+    constexpr uint c1 = s1.Count();
+
+    ASSERT_EQ(c1,               _ss1.size());
+
 	ASSERT_EQ(_s0.Count(),		0);
 	ASSERT_EQ(_s1.Count(),		_ss1.size());
 }
