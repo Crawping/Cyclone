7d3e115677e8ab71639edd875cbd16f61a48af16
JoshGrooms <josh.grooms@gmail.com>
2017-05-29 10:27:27 -0700

    Updated some tests for compatibility with the recent 'Transform3D' rename.

--- Testing/Utilities/Spatial/Transform3D.cpp
+++ Testing/Utilities/Spatial/Transform3D.cpp
@@ -94,34 +94,34 @@ struct CanonicalTransforms
 
 
 /** FIXTURE DEFINITION **/
-class _Transform : public testing::Test
+class _Transform3D : public testing::Test
 {
     protected:
         CanonicalTransforms Canon;
 
-        Transform _t0;
-        Transform _t1;
-        Transform _t2;
-        Transform _t3;
-        Transform _t4;
-        Transform _t5;
-        Transform _t6;
-        Transform _t7;
-        Transform _t8;
-        Transform _t9;
-        Transform _t10;
+        Transform3D _t0;
+        Transform3D _t1;
+        Transform3D _t2;
+        Transform3D _t3;
+        Transform3D _t4;
+        Transform3D _t5;
+        Transform3D _t6;
+        Transform3D _t7;
+        Transform3D _t8;
+        Transform3D _t9;
+        Transform3D _t10;
         
 
-        _Transform() :
+        _Transform3D() :
             Canon(),
             _t2(Canon.Position, Canon.Scale, Canon.Orientation),
-            _t4(Transform::Rotation(Canon.Orientation.X, 0.0f, 0.0f)),
-            _t5(Transform::Rotation(0.0f, Canon.Orientation.Y, 0.0f)),
-            _t6(Transform::Rotation(0.0f, 0.0f, Canon.Orientation.Z)),
-            _t7(Transform::Rotation(Canon.Orientation)),
-            _t8(Transform::Scaling(Canon.Scale)),
-            _t9(Transform::Translation(Canon.Position)),
-            _t10(Transform::Decompose(Canon.Transformation))
+            _t4(Transform3D::Rotation(Canon.Orientation.X, 0.0f, 0.0f)),
+            _t5(Transform3D::Rotation(0.0f, Canon.Orientation.Y, 0.0f)),
+            _t6(Transform3D::Rotation(0.0f, 0.0f, Canon.Orientation.Z)),
+            _t7(Transform3D::Rotation(Canon.Orientation)),
+            _t8(Transform3D::Scaling(Canon.Scale)),
+            _t9(Transform3D::Translation(Canon.Position)),
+            _t10(Transform3D::Decompose(Canon.Transformation))
         {
             _t3 = _t2;
         }
@@ -131,7 +131,7 @@ class _Transform : public testing::Test
 
 
 /** CONSTRUCTION TESTS **/
-TEST_F(_Transform, DecompositionConstructor)
+TEST_F(_Transform3D, DecompositionConstructor)
 {
     ASSERT_EQ(_t10.ToMatrix4x4(), Canon.Transformation);
     ASSERT_EQ(_t10.Position(), Canon.Position);
@@ -140,30 +140,30 @@ TEST_F(_Transform, DecompositionConstructor)
     // No easy way to test for a valid orientation here because there are many possible 
     // solutions that would result in the same overall transformation. 
 }
-TEST_F(_Transform, DefaultConstruction)
+TEST_F(_Transform3D, DefaultConstruction)
 {
     ASSERT_EQ(_t0.ToMatrix4x4(), Matrix4x4::Identity);
 }
-TEST_F(_Transform, PerspectiveProjectionConstructor)
+TEST_F(_Transform3D, PerspectiveProjectionConstructor)
 {
 
 }
-TEST_F(_Transform, RotationConstructor)
+TEST_F(_Transform3D, RotationConstructor)
 {
     ASSERT_EQ(_t4.ToMatrix4x4(), Canon.Pitch);
     ASSERT_EQ(_t5.ToMatrix4x4(), Canon.Yaw);
     ASSERT_EQ(_t6.ToMatrix4x4(), Canon.Roll);
     ASSERT_EQ(_t7.ToMatrix4x4(), Canon.Rotation);
 }
-TEST_F(_Transform, ScalingConstructor)
+TEST_F(_Transform3D, ScalingConstructor)
 {
     ASSERT_EQ(_t8.ToMatrix4x4(), Canon.Scaling);
 }
-TEST_F(_Transform, TranslationConstructor)
+TEST_F(_Transform3D, TranslationConstructor)
 {
     ASSERT_EQ(_t9.ToMatrix4x4(), Canon.Translation);
 }
-TEST_F(_Transform, VectorConstruction)
+TEST_F(_Transform3D, VectorConstruction)
 {
     ASSERT_EQ(_t2.Orientation(),    Canon.Orientation);
     ASSERT_EQ(_t2.Position(),       Canon.Position);
@@ -176,18 +176,18 @@ TEST_F(_Transform, VectorConstruction)
 
 
 /** UTILITIES TESTS **/
-TEST_F(_Transform, RelativeRotation)
+TEST_F(_Transform3D, RelativeRotation)
 {
-    Transform _t2Copy(_t2);
+    Transform3D _t2Copy(_t2);
     _t2Copy.Rotate(Canon.Orientation).Rotate(Canon.Orientation).Rotate(Canon.Orientation);
     ASSERT_FLOAT_EQ(_t2Copy.Pitch(), 3 * Canon.Orientation.X + _t2.Pitch());
     ASSERT_FLOAT_EQ(_t2Copy.Yaw(),   3 * Canon.Orientation.Y + _t2.Yaw());
     ASSERT_FLOAT_EQ(_t2Copy.Roll(),  3 * Canon.Orientation.Z + _t2.Roll());
 }
 
-TEST_F(_Transform, RelativeTranslation)
+TEST_F(_Transform3D, RelativeTranslation)
 {
-    Transform _t2Copy(_t2);
+    Transform3D _t2Copy(_t2);
     _t2Copy.Translate(Canon.Position).Translate(Canon.Position).Translate(Canon.Position);
     ASSERT_FLOAT_EQ(_t2Copy.X(), 3 * Canon.Position.X + _t2.X());
     ASSERT_FLOAT_EQ(_t2Copy.Y(), 3 * Canon.Position.Y + _t2.Y());
@@ -197,16 +197,16 @@ TEST_F(_Transform, RelativeTranslation)
 
 
 /** OPERATOR TESTS **/
-TEST_F(_Transform, EqualityOperators)
+TEST_F(_Transform3D, EqualityOperators)
 {
     ASSERT_EQ(_t0, _t0);
     ASSERT_EQ(_t0, _t1);
     ASSERT_NE(_t0, _t2);
 }
 
-TEST_F(_Transform, MultiplicationOperators)
+TEST_F(_Transform3D, MultiplicationOperators)
 {
-    Transform _catRotation = _t6 * _t5 * _t4;    
+    Transform3D _catRotation = _t6 * _t5 * _t4;    
     ASSERT_EQ(_catRotation.ToMatrix4x4(), Canon.Rotation);
     ASSERT_EQ(_t7, _catRotation);
 }
