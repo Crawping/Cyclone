70a983b55b5f3f580a828214567902e04a6b5a59
JoshGrooms <josh.grooms@gmail.com>
2017-06-01 16:23:48 -0700

    Wrote a few new helpful math utilities.

--- Testing/Utilities/Math/Math.cpp
+++ Testing/Utilities/Math/Math.cpp
@@ -9,6 +9,16 @@ using namespace Cyclone::Utilities;
 
 
 
+TEST(_Math, Abs)
+{
+    constexpr double v1 = Math::Abs(-3.14159265);
+    constexpr float v2 = Math::Abs(0.0f);
+    constexpr int v3 = Math::Abs(123456789);
+
+    ASSERT_EQ(v1, 3.14159265);
+    ASSERT_EQ(v2, 0.0f);
+    ASSERT_EQ(v3, 123456789);
+}
 TEST(_Math, Clamping)
 {
     constexpr int x = -10;
@@ -41,8 +51,52 @@ TEST(_Math, Extrema)
     ASSERT_EQ(m3, 10);
     ASSERT_EQ(m4, m3);
 }
+TEST(_Math, IsBetween)
+{
+    constexpr bool v1 = Math::IsBetween(1, 0, 10);
+    constexpr bool v2 = Math::IsBetween(1000, 100, 1000000);
+    constexpr bool v3 = Math::IsBetween(-20, -21, -19);
+    constexpr bool v4 = Math::IsBetween(-1, -5, 10);
+
+    ASSERT_EQ(v1, true);
+    ASSERT_EQ(v2, true);
+    ASSERT_EQ(v3, true);
+    ASSERT_EQ(v4, true);
+
+    constexpr bool v5 = Math::IsBetween(-1, 0, 10);
+    constexpr bool v6 = Math::IsBetween(10, 1, 9);
+    constexpr bool v7 = Math::IsBetween(-1000000, -10, 30);
+
+    ASSERT_EQ(v5, false);
+    ASSERT_EQ(v6, false);
+    ASSERT_EQ(v7, false);
+}
 TEST(_Math, Product)
 {
     constexpr int v1 = Math::Product(1, 2, 3, 4);
+    constexpr int v2 = Math::Product(10);
+
     ASSERT_EQ(v1, 24);
+    ASSERT_EQ(v2, 10);
+}
+TEST(_Math, Sign)
+{
+    constexpr int v1 = Math::Sign(-3.14159265);
+    constexpr int v2 = Math::Sign(0.0f);
+    constexpr int v3 = Math::Sign(123456789);
+
+    ASSERT_EQ(v1, -1);
+    ASSERT_EQ(v2,  1);
+    ASSERT_EQ(v3,  1);
+}
+TEST(_Math, Sum)
+{
+    constexpr int v1 = Math::Sum(1, 2, 3, 4, 5, 6, 7, 8, 9);
+    constexpr int v2 = Math::Sum(-1, -2, -3, -4, -5, -6, -7, -8, -9);
+
+    ASSERT_EQ(v1, 45);
+    ASSERT_EQ(v2, -45);
+
+    constexpr int v3 = Math::Sum(1);
+    ASSERT_EQ(v3, 1);
 }
--- Utilities/Math/Math.h
+++ Utilities/Math/Math.h
@@ -13,8 +13,10 @@ namespace Cyclone
     {
         namespace Math
         {
-            /// <summary> Constrains numeric or comparable data to a range of values between some inputted minimum and maximum. </summary>
-            /// <param name="x"> The data point to be constrained. </param>
+            /// <summary> Returns the absolute value of the input. </summary>
+            template<typename T> constexpr T Abs(T x) { return (x < 0) ? -x : x; }
+            /// <summary> Constrains data to a range of values between some inputted minimum and maximum. </summary>
+            /// <param name="x"> The value to be constrained. </param>
             /// <param name="min"> The minimum value that <paramref name="x"/> is allowed to take. </param>
             /// <param name="max"> The maximum value that <paramref name="x"/> is allowed to take. </param>
             /// <returns> 
@@ -26,10 +28,31 @@ namespace Cyclone
                 return (x < min) ? min :
                     (x > max) ? max : x;
             }
-            template<typename T> constexpr int Compare(const T& x, const T& y)
+            /// <summary> Determines the ordering relationship between two values. </summary>
+            /// <returns> 
+            ///     A <c>-1</c> if <paramref name="x"/> is less than <paramref name="y"/>, a <c>0</c> if they are equal, 
+            ///     or a <c>1</c> if <paramref name="x"/> is greater than <paramref name="y"/>.
+            /// </returns>
+            /// <param name="x"> The first value. </param>
+            /// <param name="y"> The second value. </param>
+            template<typename T> constexpr int Compare(T x, T y)
             {
                 return (x < y) ? -1 :
                     (x > y) ? 1 : 0;
+            }
+            /// <summary> Determines whether a value lies within a range of values. </summary>
+            /// <returns>
+            ///     A Boolean <c>true</c> if <paramref name="x"/> lies between <paramrefname="min"/> and <paramref name="max"/>, 
+            ///     or <c>false</c> otherwise.
+            /// </returns>
+            /// <param name="x"> The value to be tested. </param>
+            /// <param name="min"> The lower bound of the range of values. </param>
+            /// <param name="max"> The upper bound of the range of values. </param>
+            /// <param name="inclusive"> An value of <c>true</c> if the bounds should be considered inside the range, or <c>false</c> otherwise. </param>
+            template<typename T> constexpr bool IsBetween(T x, T min, T max, bool inclusive = true)
+            {
+                return inclusive ? (x >= min && x <= max) :
+                    (x > min && x < max);
             }
 
             /// <summary> Returns the maximum of a list of values. </summary>
@@ -71,8 +94,31 @@ namespace Cyclone
 
                 return v;
             }
-            template<typename T> constexpr T Product(T first) { return first; }
+            /// <summary> Returns the inputted value. </summary>
+            /// <remarks> This function exists as a stop condition for the variadic template of the same name. </remarks>
+            template<typename T> constexpr T Product(T first)   { return first; }
+            /// <summary> Returns the sign of a value. </summary>
+            /// <returns> A <c>-1</c> if the value is less than zero, or <c>1</c> otherwise. </returns>
+            /// <param name="x"> The value being tested. </param>
+            template<typename T> constexpr int Sign(T x)        { return (x < 0) ? -1 : 1; }
+            /// <summary> Returns the cumulative sum of a list of values. </summary>
+            /// <returns></returns>
+            /// <param name="first"> The first value of the list being added. </param>
+            /// <param name="...values"> Any number of other values to be added with the first. </param>
+            template<typename T, typename ... U> constexpr T Sum(T first, U ... values)
+            {
+                constexpr int N = sizeof...(U);
+                const T vals[N] = { values... };
 
+                T v = first;
+                for (int a = 0; a < N; a++)
+                    v += vals[a];
+
+                return v;
+            }
+            /// <summary> Returns the inputted value. </summary>
+            /// <remarks> This function exists to prevent the variadic overload from trying to create empty arrays. </remarks>
+            template<typename T> constexpr T Sum(T first)       { return first; }
         }
     }
 }
