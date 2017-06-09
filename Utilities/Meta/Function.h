bc441cb8e30a2a89d54553178cfa92a76a2a1441
JoshGrooms <josh.grooms@gmail.com>
2017-06-09 05:58:04 -0700

    Added some new meta-utilities for deducing function arguments.

--- Utilities/Meta/Function.h
+++ Utilities/Meta/Function.h
@@ -21,6 +21,17 @@ namespace Cyclone
                 using IsPointer     = Boolean<true>;
                 using Output        = T;
             };
+
+            template<typename T> struct InputTypes              { };
+            template<typename T, typename U, typename ... V>
+            struct InputTypes< MethodPointer<T, U, V...> >:     List<V...> { using Value = List<V...>; };
+
+            template<typename T> struct ReturnType              { };
+            template<typename T, typename U, typename ...V>
+            struct ReturnType< MethodPointer<T, U, V...> >      { using Value = T; };
+
+            template<typename T, typename ... U>
+            struct ReturnType< ICallback<T, U...> >             { using Value = T; };
         }
     }
 }
