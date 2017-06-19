53303085445f8369257a67f164163b337cb8fbc4
JoshGrooms <josh.grooms@gmail.com>
2017-06-19 10:32:47 -0700

    Consolidated some 'ResourceLibrary' methods.

--- OpenGL/Resources/ResourceLibrary.cpp
+++ OpenGL/Resources/ResourceLibrary.cpp
@@ -15,6 +15,7 @@ namespace Cyclone
             _materials.Insert("Null", nullptr);
             _pipelines.Insert("Null", nullptr);
             _renderables.Insert("Null", nullptr);
+            _settings.Insert("Null", nullptr);
             _textures.Insert("Null", nullptr);
         }
         ResourceLibrary2::~ResourceLibrary2()
@@ -24,65 +25,45 @@ namespace Cyclone
             for (auto v : _materials.Values())      { delete v; }
             for (auto v : _pipelines.Values())      { delete v; }
             for (auto v : _renderables.Values())    { delete v; }
+            for (auto v : _settings.Values())       { delete v; }
             for (auto v : _textures.Values())       { delete v; }
         }
 
 
 
         /** PRIVATE UTILITIES **/
-        bool ResourceLibrary2::Contains(const string& key, IGraphicsBuffer* value)    const
+        void ResourceLibrary2::Insert(const string& key, IGraphicsBuffer* value)
         {
-            return _buffers.Contains(key) && _buffers[key] == value;
-        }
-        bool ResourceLibrary2::Contains(const string& key, IGeometric* value)         const
-        {
-            return _geometry.Contains(key) && _geometry[key] == value;
-        }
-        bool ResourceLibrary2::Contains(const string& key, IMaterial* value)          const
-        {
-            return _materials.Contains(key) && _materials[key] == value;
-        }
-        bool ResourceLibrary2::Contains(const string& key, GraphicsPipeline* value)   const
-        {
-            return _pipelines.Contains(key) && _pipelines[key] == value;
-        }
-        bool ResourceLibrary2::Contains(const string& key, IRenderable* value)        const
-        {
-            return _renderables.Contains(key) && _renderables[key] == value;
-        }
-        bool ResourceLibrary2::Contains(const string& key, ITexture* value)           const
-        {
-            return _textures.Contains(key) && _textures[key] == value;
-        }
-
-        void ResourceLibrary2::Register(const string& key, IGraphicsBuffer* value)
-        {
-            if (_buffers.Contains(key)) { delete _buffers[key]; }
+            //if (_buffers.Contains(key)) { delete _buffers[key]; }
             _buffers.Insert(key, value);
         }
-        void ResourceLibrary2::Register(const string& key, GraphicsPipeline* value)
+        void ResourceLibrary2::Insert(const string& key, GraphicsPipeline* value)
         {
-            if (_pipelines.Contains(key)) { delete _pipelines[key]; }
+            //if (_pipelines.Contains(key)) { delete _pipelines[key]; }
             _pipelines.Insert(key, value);
         }
-        void ResourceLibrary2::Register(const string& key, IMaterial* value)
+        void ResourceLibrary2::Insert(const string& key, GraphicsSettings* value)
         {
-            if (_materials.Contains(key)) { delete _materials[key]; }
+            _settings.Insert(key, value);
+        }
+        void ResourceLibrary2::Insert(const string& key, IMaterial* value)
+        {
+            //if (_materials.Contains(key)) { delete _materials[key]; }
             _materials.Insert(key, value);
         }
-        void ResourceLibrary2::Register(const string& key, IGeometric* value)
+        void ResourceLibrary2::Insert(const string& key, IGeometric* value)
         {
-            if (_geometry.Contains(key)) { delete _geometry[key]; }
+            //if (_geometry.Contains(key)) { delete _geometry[key]; }
             _geometry.Insert(key, value);
         }
-        void ResourceLibrary2::Register(const string& key, IRenderable* value)
+        void ResourceLibrary2::Insert(const string& key, IRenderable* value)
         {
-            if (_renderables.Contains(key)) { delete _renderables[key]; }
+            //if (_renderables.Contains(key)) { delete _renderables[key]; }
             _renderables.Insert(key, value);
         }
-        void ResourceLibrary2::Register(const string& key, ITexture* value)
+        void ResourceLibrary2::Insert(const string& key, ITexture* value)
         {
-            if (_textures.Contains(key)) { delete _textures[key]; }
+            //if (_textures.Contains(key)) { delete _textures[key]; }
             _textures.Insert(key, value);
         }
 
--- OpenGL/Resources/ResourceLibrary2.h
+++ OpenGL/Resources/ResourceLibrary2.h
@@ -3,6 +3,7 @@
  */
 
 #pragma once
+#include "GraphicsSettings.h"
 #include "Collections/BST.h"
 #include "Interfaces/ICallback.h"
 #include "Interfaces/IGeometric.h"
@@ -28,13 +29,16 @@ namespace Cyclone
                 uint BufferCount()      const { return _buffers.Count() - 1; }
                 uint Count()            const 
                 { 
-                    return BufferCount() + GeometryCount() + MaterialCount() + 
-                        PipelineCount() + RenderableCount() + TextureCount(); 
+                    return 
+                        BufferCount() + GeometryCount() + MaterialCount() + 
+                        PipelineCount() + RenderableCount() + TextureCount() + 
+                        SettingsCount();
                 }
                 uint GeometryCount()    const { return _geometry.Count() - 1; }
                 uint MaterialCount()    const { return _materials.Count() - 1; }
                 uint PipelineCount()    const { return _pipelines.Count() - 1; }
                 uint RenderableCount()  const { return _renderables.Count() - 1; }
+                uint SettingsCount()    const { return _settings.Count() - 1; }
                 uint TextureCount()     const { return _textures.Count() - 1; }
                 
 
@@ -52,7 +56,8 @@ namespace Cyclone
                 /// <param name="value"> An existing resource handle. </param>
                 template<typename T> bool Contains(Resource<T> value) const 
                 {
-                    return Contains(value.Name(), value._value);
+                    auto r = Get<T>(value.Name());
+                    return !r.IsNull() && (r._value == value._value);
                 }
                 /// <summary> Determines whether a particular resource is stored within the library. </summary>
                 /// <returns> A Boolean <c>true</c> if the resource is stored within the library, or <c>false</c> otherwise. </returns>
@@ -66,38 +71,39 @@ namespace Cyclone
                         Meta::IsA<T, IMaterial>()           ? _materials.Contains(name)     :
                         Meta::IsA<T, GraphicsPipeline>()    ? _pipelines.Contains(name)     :
                         Meta::IsA<T, IRenderable>()         ? _renderables.Contains(name)   : 
+                        Meta::IsA<T, GraphicsSettings>()    ? _settings.Contains(name)      :
                         Meta::IsA<T, ITexture>()            ? _textures.Contains(name)      : false;
                 }
                 /// <summary> Creates a new graphics resource that can be used on the GPU. </summary>
                 template<typename T> Resource<T> Create(const string& name)
                 {
                     T* value = new T();
-                    Register(name, value);
-                    return Resource<T>(name, value);
+                    return Register<T>(name, value);
                 }
                 /// <summary> Creates a new graphics resource that can be used on the GPU. </summary>
                 template<typename T, typename ... U>
                 Resource<T> Create(const string& name, const ICallback<T, U...>& constructor, U ... arguments)
                 {
                     T* value = new T(constructor(arguments...));
-                    Register(name, value);
-                    return Resource<T>(name, value);
+                    return Register<T>(name, value);
                 }
                 /// <summary> Destroys an existing graphics resource that is stored within the library. </summary>
                 template<typename T> void Destroy(Resource<T> value)
                 {
                     const string& key = value.Name();
-                    if (value.IsNull() || !Contains(value)) { return; }
+                    if ( value.IsNull() || !Contains(value) ) { return; }
+
                     Meta::IsA<T, IGraphicsBuffer>()     ? _buffers.Remove(key)      :
                     Meta::IsA<T, IGeometric>()          ? _geometry.Remove(key)     :
                     Meta::IsA<T, IMaterial>()           ? _materials.Remove(key)    :
                     Meta::IsA<T, GraphicsPipeline>()    ? _pipelines.Remove(key)    : 
-                    Meta::IsA<T, IRenderable>()         ? _renderables.Remove(key)  : _textures.Remove(value.Name());
+                    Meta::IsA<T, IRenderable>()         ? _renderables.Remove(key)  : 
+                    Meta::IsA<T, GraphicsSettings>()    ? _settings.Remove(key)     : _textures.Remove(key);
 
                     delete value._value;
                 }
                 /// <summary> Acquires a particular graphics resource stored within the library. </summary>
-                template<typename T> Resource<T> Get(const string& name)
+                template<typename T> Resource<T> Get(const string& name) const
                 {
                     return
                         !Contains<T>(name)                  ? Resource<T>(name, nullptr)                                : 
@@ -106,9 +112,23 @@ namespace Cyclone
                         Meta::IsA<T, IGeometric>()          ? Resource<T>(name, dynamic_cast<T*>(_geometry[name]))      :
                         Meta::IsA<T, GraphicsPipeline>()    ? Resource<T>(name, dynamic_cast<T*>(_pipelines[name]))     :
                         Meta::IsA<T, IRenderable>()         ? Resource<T>(name, dynamic_cast<T*>(_renderables[name]))   : 
-                        Resource<T>(name, dynamic_cast<T*>(_textures[name]));
+                        Meta::IsA<T, GraphicsSettings>()    ? Resource<T>(name, dynamic_cast<T*>(_settings[name]))      :
+                        Meta::IsA<T, ITexture>()            ? Resource<T>(name, dynamic_cast<T*>(_textures[name]))      : 
+                        Resource<T>(name, nullptr);
                 }
+                template<typename T> Resource<T> Register(const string& name, T* value)
+                {
+                    if (!value || (name == "Null"))
+                        return Resource<T>(name, nullptr);
 
+                    auto r = Get<T>(name);
+                    if (!r.IsNull() && r._value != value)
+                        delete r._value;
+
+                    Insert(name, value);
+                    return Resource<T>(name, value);
+                }
+                
             private:
 
                 /** DATA **/
@@ -117,24 +137,19 @@ namespace Cyclone
                 BST<string, IMaterial*>         _materials;
                 BST<string, GraphicsPipeline*>  _pipelines;
                 BST<string, IRenderable*>       _renderables;
+                BST<string, GraphicsSettings*>  _settings;
                 BST<string, ITexture*>          _textures;
 
 
 
                 /** UTILITIES **/
-                OpenGLAPI bool Contains(const string& key, IGraphicsBuffer* value)    const;
-                OpenGLAPI bool Contains(const string& key, IGeometric* value)         const;
-                OpenGLAPI bool Contains(const string& key, IMaterial* value)          const;
-                OpenGLAPI bool Contains(const string& key, GraphicsPipeline* value)   const;
-                OpenGLAPI bool Contains(const string& key, IRenderable* value)        const;
-                OpenGLAPI bool Contains(const string& key, ITexture* value)           const;
-
-                OpenGLAPI void Register(const string& key, IGraphicsBuffer* value);
-                OpenGLAPI void Register(const string& key, GraphicsPipeline* value);
-                OpenGLAPI void Register(const string& key, IMaterial* value);
-                OpenGLAPI void Register(const string& key, IGeometric* value);
-                OpenGLAPI void Register(const string& key, IRenderable* value);
-                OpenGLAPI void Register(const string& key, ITexture* value);
+                OpenGLAPI void Insert(const string& key, IGraphicsBuffer* value);
+                OpenGLAPI void Insert(const string& key, GraphicsPipeline* value);
+                OpenGLAPI void Insert(const string& key, GraphicsSettings* value);
+                OpenGLAPI void Insert(const string& key, IMaterial* value);
+                OpenGLAPI void Insert(const string& key, IGeometric* value);
+                OpenGLAPI void Insert(const string& key, IRenderable* value);
+                OpenGLAPI void Insert(const string& key, ITexture* value);
 
         };
     }
