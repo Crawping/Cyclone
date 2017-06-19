a95abcef7f64e1731012475be3fe6e7098f135f1
JoshGrooms <josh.grooms@gmail.com>
2017-06-19 05:53:28 -0700

    Moved some 'Entity3D' method implementations over to a new source file.

---
+++ OpenGL/Geometry/Entity3D.cpp
@@ -0,0 +1,86 @@
+#include "Geometry/Entity3D.h"
+
+
+
+namespace Cyclone
+{
+    namespace OpenGL
+    {
+
+        /** GEOMETRY PROPERTIES **/
+        Entity3D& Entity3D::Geometry(Resource<Geometry3D> value)
+        {
+            _model.Geometry(value);
+            return *this;
+        }
+        Entity3D& Entity3D::Orientation(const Vector3& value)
+        {
+            _transform.Orientation(value);
+            return *this;
+        }
+        Entity3D& Entity3D::Position(const Vector3& value)
+        {
+            _transform.Position(value);
+            return *this;
+        }
+        Entity3D& Entity3D::Scale(const Vector3& value)
+        {
+            _transform.Scale(value);
+            return *this;
+        }
+
+
+
+        /** MATERIAL PROPERTIES **/
+        Entity3D& Entity3D::Ambience(float value)
+        {
+            _material.Ambience(value);
+            return *this;
+        }
+        Entity3D& Entity3D::PrimaryColor(const Color4& value)
+        {
+            _material.PrimaryColor(value);
+            return *this;
+        }
+        Entity3D& Entity3D::SecondaryColor(const Color4& value)
+        {
+            _material.SecondaryColor(value);
+            return *this;
+        }
+        Entity3D& Entity3D::SpecularPower(float value)
+        {
+            _material.SpecularPower(value);
+            return *this;
+        }
+        Entity3D& Entity3D::Texture(Resource<ITexture> value)
+        {
+            _material.Texture(value);
+            return *this;
+        }
+
+
+
+        /** IRENDERABLE PROPERTIES **/
+        Entity3D& Entity3D::IsVisible(bool value)
+        {
+            _isVisible = value;
+            return *this;
+        }
+        Entity3D& Entity3D::Material(const Material3D& value)
+        {
+            _material = value;
+            return *this;
+        }
+        Entity3D& Entity3D::Model(const Model3D& value)
+        {
+            _model = value;
+            return *this;
+        }
+        Entity3D& Entity3D::Transform(const Transform3D& value)
+        {
+            _transform = value;
+            return *this;
+        }
+
+    }
+}
--- OpenGL/Geometry/Entity3D.h
+++ OpenGL/Geometry/Entity3D.h
@@ -4,6 +4,7 @@
 
 #pragma once
 #include "Interfaces/IRenderable.h"
+#include "IO/Event.h"
 #include "Libraries/Material3D.h"
 #include "Models/Model3D.h"
 #include "Resources/Resource.h"
@@ -32,26 +33,10 @@ namespace Cyclone
                 const Vector3& Position()                               const override { return _transform.Orientation(); }
                 const Vector3& Scale()                                  const override { return _transform.Orientation(); }
 
-                Entity3D& Geometry(const Geometry3D& value)
-                {
-                    _model.Geometry(value);
-                    return *this;
-                }
-                Entity3D& Orientation(const Vector3& value)             override
-                {
-                    _transform.Orientation(value);
-                    return *this;
-                }
-                Entity3D& Position(const Vector3& value)                override
-                {
-                    _transform.Position(value);
-                    return *this;
-                }
-                Entity3D& Scale(const Vector3& value)                   override
-                {
-                    _transform.Scale(value);
-                    return *this;
-                }
+                OpenGLAPI Entity3D& Geometry(Resource<Geometry3D> value);
+                OpenGLAPI Entity3D& Orientation(const Vector3& value)             override;
+                OpenGLAPI Entity3D& Position(const Vector3& value)                override;
+                OpenGLAPI Entity3D& Scale(const Vector3& value)                   override;
 
 
 
@@ -66,38 +51,18 @@ namespace Cyclone
                 /// <summary> Gets a pointer to the texture associated with an entity. </summary>
 		        //virtual const Texture3D* Texture()                      const { return Material().Texture(); }
                 //virtual Resource<Texture3D> Texture()                   const { return Material().Texture(); }
-                virtual Resource<ITexture> Texture()                    const { return Material().Texture(); }
+                //virtual Resource<ITexture> Texture()                    const { return Material().Texture(); }
+                virtual const ITexture* Texture()                       const { return Material().Texture(); }
 
-                virtual Entity3D& Ambience(float value)
-                {
-                    _material.Ambience(value);
-                    return *this;
-                }
+                OpenGLAPI virtual Entity3D& Ambience(float value);
                 /// <summary> Sets the primary color of the entity. </summary>
-                virtual Entity3D& PrimaryColor(const Color4& value)
-                { 
-                    _material.PrimaryColor(value);
-                    return *this;
-                }
+                OpenGLAPI virtual Entity3D& PrimaryColor(const Color4& value);
                 /// <summary> Sets the secondary color of the entity. </summary>
-                virtual Entity3D& SecondaryColor(const Color4& value)
-                {
-                    _material.SecondaryColor(value);
-                    return *this;
-                }
-                virtual Entity3D& SpecularPower(float value)
-                {
-                    _material.SpecularPower(value);
-                    return *this;
-                }
+                OpenGLAPI virtual Entity3D& SecondaryColor(const Color4& value);
+
+                OpenGLAPI virtual Entity3D& SpecularPower(float value);
                 /// <summary> Sets the texture to be used when rendering the entity. </summary>
-                virtual Entity3D& Texture(Resource<ITexture> value)
-                //virtual Entity3D& Texture(const Resource<Texture3D>& value)
-                //virtual Entity3D& Texture(Texture3D* value)
-                {
-                    _material.Texture(value);
-                    return *this;
-                }
+                OpenGLAPI virtual Entity3D& Texture(Resource<ITexture> value);
 
 
                 
@@ -105,7 +70,6 @@ namespace Cyclone
                 /// <summary> Gets whether the entity is visible in the rendered environment. </summary>
                 bool IsVisible()                                        const override { return _isVisible; }
                 /// <summary> Gets the material used to style the entity in the rendered environment. </summary>
-                //Resource<IMaterial> Material()                          const override { return _material; }
                 const Material3D& Material()                            const override { return _material; }
 
                 const Model3D& Model()                                  const override { return _model; }
@@ -115,31 +79,18 @@ namespace Cyclone
 
 
                 /// <summary> Sets whether the entity is visible in a rendered environment. </summary>
-                virtual Entity3D& IsVisible(bool value)
-                {
-                    _isVisible = value;
-                    return *this;
-                }
+                OpenGLAPI virtual Entity3D& IsVisible(bool value);
                 /// <summary> Sets the material used to style the entity in the rendered environment. </summary>
-                virtual Entity3D& Material(const Material3D& value)
-                //virtual Entity3D& Material(Resource<IMaterial> value)
-                { 
-                    _material = value;
-                    return *this;
-                }
+                OpenGLAPI virtual Entity3D& Material(const Material3D& value);
 
-                virtual Entity3D& Model(const Model3D& value)
-                {
-                    _model = value;
-                    return *this;
-                }
+                OpenGLAPI virtual Entity3D& Model(const Model3D& value);
 
-                virtual Entity3D& Transform(const Transform3D& value)
-                {
-                    _transform = value;
-                    return *this;
-                }
+                OpenGLAPI virtual Entity3D& Transform(const Transform3D& value);
 
+
+
+                /** EVENTS **/
+                //Event<
 
 
                 /** CONSTRUCTOR & DESTRUCTOR **/
@@ -160,7 +111,6 @@ namespace Cyclone
                 
                 /** PROPERTY DATA **/
                 bool                _isVisible;
-                //Resource<IMaterial> _material;
                 Material3D          _material;
                 Model3D             _model;
                 Transform3D         _transform;
