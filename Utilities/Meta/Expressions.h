db6000cb980286a3050507e07d02673e79b5cc88
JoshGrooms <josh.grooms@gmail.com>
2017-07-12 14:52:05 -0700

    Completely overhauled the new 'Expressions' file to improve usage.

--- Utilities/Meta/Expressions.h
+++ Utilities/Meta/Expressions.h
@@ -18,32 +18,52 @@ namespace Cyclone
             constexpr void Expand(T&&...)           { }
 
 
+
             template<typename T, typename U, typename ... V>
-            struct IsRelated:                      Conditional< IsRelative<T, U>::Value, Boolean<true>, IsRelated<T, V...> > { };
+            struct IsRelated:                       Conditional< IsRelative<T, U>::Value, Boolean<true>, IsRelated<T, V...> > { };
             template<typename T, typename U>
-            struct IsRelated<T, U>:                IsRelative<T, U> { };
+            struct IsRelated<T, U>:                 IsRelative<T, U> { };
+
+            template<typename T, typename U, typename ... V>
+            struct IsSubtype:                       Conditional< IsA<T, U>::Value, Boolean<true>, IsSubtype<T, V...> > { };
+            template<typename T, typename U>
+            struct IsSubtype<T, U>:                 IsA<T, U> { };
 
 
 
             template<bool S, typename T = void>
-            struct EnableIf                         { };
+            struct Disable                          { using Type = T; };
+
+            template<typename T> 
+            struct Disable<false, T>                { };
+
+            template<bool S, typename T = void>
+            struct Enable                           { };
 
             template<typename T>
-            struct EnableIf<true, T>                { using Type = T; };
+            struct Enable<true, T>                  { using Type = T; };
+
+
+
+            template<typename T, typename U = void>
+            using DisableIf                         = typename Disable<T::Value, U>::Type*;
+        
+            template<typename T, typename U = void>
+            using EnableIf                          = typename Enable<T::Value, U>::Type*;
 
 
 
             template<typename T, typename ... U>
-            using EnableRelatives                   = typename EnableIf< IsRelated<T, U...>::Value >::Type*;
-
-            template<typename T, typename ... U>
-            using EnableTypes                       = typename EnableIf< IsMember<T, U...>::Value >::Type*;
-
-            template<typename T, typename ... U>
-            using DisableRelatives                  = typename EnableIf< !IsRelated<T, U...>::Value >::Type*;
+            using DisableRelatives                  = typename DisableIf< IsRelated<T, U...> >;
             
             template<typename T, typename ... U>
-            using DisableTypes                      = typename EnableIf< !IsMember<T, U...>::Value >::Type*;
+            using DisableTypes                      = typename DisableIf< IsMember<T, U...> >;
+
+            template<typename T, typename ... U>
+            using EnableRelatives                   = typename EnableIf< IsRelated<T, U...> >;
+
+            template<typename T, typename ... U>
+            using EnableTypes                       = typename EnableIf< IsMember<T, U...> >;
 
         }
     }
